809e70eec46e5825eb2f6ae6cff2077d1181279a
Samuel Holland <samuel@sholland.org>
2017-10-18 23:08:59 -0500

    include: Add C wrappers for l.mfspr/l.mtspr
    These functions require the SPR address to be known at compile time, as
    it is encoded as an immediate value in the instruction.
    
    Signed-off-by: Samuel Holland <samuel@sholland.org>

---
+++ include/spr.h
@@ -0,0 +1,28 @@
+/*
+ * Copyright © 2017 Samuel Holland <samuel@sholland.org>
+ * SPDX-License-Identifier: (BSD-3-Clause OR GPL-2.0)
+ */
+
+#ifndef SPR_H
+#define SPR_H
+
+#include <compiler.h>
+#include <stdint.h>
+#include <arch/spr.h>
+
+static inline uint32_t
+mfspr(uint16_t addr)
+{
+	uint32_t value;
+
+	asm volatile ("l.mfspr %0, r0, %1" : "=r" (value) : "K" (addr));
+	return value;
+}
+
+static inline void
+mtspr(uint16_t addr, uint32_t value)
+{
+	asm volatile ("l.mtspr r0, %1, %0" : : "K" (addr), "r" (value));
+}
+
+#endif /* SPR_H */
