68ba12e146e93c9b1207886c847dfce90ee8761f
Samuel Holland <samuel@sholland.org>
2017-10-23 15:25:49 -0500

    include: Add a list of error codes
    These are in the style of errno, but more appropriate for the kinds of
    failure possible here. To avoid the possibility of forgetting minus
    signs everywhere, they are defined to be negative from the start.
    
    Signed-off-by: Samuel Holland <samuel@sholland.org>

---
+++ include/error.h
@@ -0,0 +1,18 @@
+/*
+ * Copyright © 2017 Samuel Holland <samuel@sholland.org>
+ * SPDX-License-Identifier: (BSD-3-Clause OR GPL-2.0)
+ */
+
+#ifndef ERROR_H
+#define ERROR_H
+
+#define SUCCESS 0    /* The operation succeeded. */
+#define EAGAIN  (-1) /* Try again immediately. */
+#define EBUSY   (-2) /* Try again later. */
+#define EEXIST  (-3) /* The object already exists. */
+#define EIO     (-4) /* Communication with the hardware failed. */
+#define ENOBUS  (-5) /* The bus for this device does not exist. */
+#define ENODEV  (-6) /* This device does not exist. */
+#define ENOTSUP (-7) /* The operation is not supported. */
+
+#endif /* ERROR_H */
