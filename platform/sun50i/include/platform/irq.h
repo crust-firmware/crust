26c96e4104fc6d09eb5cb096b62661c53a63b415
Samuel Holland <samuel@sholland.org>
2017-10-23 15:43:07 -0500

    platform: sun50i: Add platform IRQ assignments
    Signed-off-by: Samuel Holland <samuel@sholland.org>

---
+++ platform/sun50i/include/platform/irq.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright © 2017 Samuel Holland <samuel@sholland.org>
+ * SPDX-License-Identifier: (BSD-3-Clause OR GPL-2.0)
+ */
+
+#ifndef PLATFORM_IRQ_H
+#define PLATFORM_IRQ_H
+
+#define IRQ_NMI    0x00
+#define IRQ_TIMER0 0x01
+#define IRQ_TIMER1 0x02
+
+#define IRQ_MSGBOX 0x11
+
+#endif /* PLATFORM_IRQ_H */
