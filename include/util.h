09588f2a41d9e47a93c1aec533d29581a79d3227
Samuel Holland <samuel@sholland.org>
2017-10-23 15:41:14 -0500

    include: Expand utility macro definitions
    Add the ever-useful ARRAY_SIZE macro, as well as some additional
    bit-manipulation macros. These are useful for storing and using
    indexes into bitmaps.
    
    Signed-off-by: Samuel Holland <samuel@sholland.org>

--- include/util.h
+++ include/util.h
@@ -6,12 +6,22 @@
 #ifndef UTIL_H
 #define UTIL_H
 
+#include <limits.h>
+
 #ifdef __ASSEMBLER__
-#define U(n)   (n)
+#define U(n)                    (n)
 #else
-#define U(n)   (n ## U)
+#define U(n)                    (n ## U)
 #endif
 
-#define BIT(n) (U(1) << (n))
+#define ARRAY_SIZE(a)           (sizeof(a) / sizeof((a)[0]))
+
+#define BIT(n)                  (U(1) << (n))
+#define BITMASK(off, len)       ((BIT(len) - 1) << (off))
+
+#define BITMAP_INDEX(word, bit) (WORD_BIT * (word) + (bit))
+#define BITMAP_WORD(index)      ((index) / WORD_BIT)
+#define BITMAP_WORDOFF(index)   (BITMAP_WORD(index) * (WORD_BIT / CHAR_BIT))
+#define BITMAP_BIT(index)       ((index) % WORD_BIT)
 
 #endif /* UTIL_H */
