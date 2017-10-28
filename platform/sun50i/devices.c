95fbefb7d9b81954984b0bf0504c9a5dc570b300
Samuel Holland <samuel@sholland.org>
2017-10-28 04:18:58 -0500

    platform: sun50i: Add MSGBOX device
    Signed-off-by: Samuel Holland <samuel@sholland.org>

--- platform/sun50i/devices.c
+++ platform/sun50i/devices.c
@@ -11,14 +11,26 @@
 
 extern struct driver sun4i_intc_driver;
 extern struct driver sunxi_ccu_driver;
+extern struct driver sunxi_msgbox_driver;
 
 static struct device ccu    __device;
+static struct device msgbox __device;
 static struct device r_intc __device;
 
 static struct device ccu = {
 	.name    = "ccu",
 	.address = DEV_CCU,
 	.drv     = &sunxi_ccu_driver,
+};
+
+static struct device msgbox = {
+	.name     = "msgbox",
+	.address  = DEV_MSGBOX,
+	.clock    = CCU_GATE(CCU_GATE_MSGBOX) | CCU_RESET(CCU_RESET_MSGBOX),
+	.clockdev = &ccu,
+	.drv      = &sunxi_msgbox_driver,
+	.irq      = IRQ_MSGBOX,
+	.irqdev   = &r_intc,
 };
 
 static struct device r_intc = {
