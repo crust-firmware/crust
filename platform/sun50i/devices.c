03551241e173e70dd7547eded6491fc1c5862d4a
Samuel Holland <samuel@sholland.org>
2017-10-28 04:17:12 -0500

    platform: sun50i: Add R_INTC device
    Signed-off-by: Samuel Holland <samuel@sholland.org>

--- platform/sun50i/devices.c
+++ platform/sun50i/devices.c
@@ -7,13 +7,22 @@
 #include <drivers/clock/sunxi-ccu.h>
 #include <platform/ccu.h>
 #include <platform/devices.h>
+#include <platform/irq.h>
 
+extern struct driver sun4i_intc_driver;
 extern struct driver sunxi_ccu_driver;
 
-static struct device ccu __device;
+static struct device ccu    __device;
+static struct device r_intc __device;
 
 static struct device ccu = {
 	.name    = "ccu",
 	.address = DEV_CCU,
 	.drv     = &sunxi_ccu_driver,
+};
+
+static struct device r_intc = {
+	.name    = "r_intc",
+	.address = DEV_R_INTC,
+	.drv     = &sun4i_intc_driver,
 };
