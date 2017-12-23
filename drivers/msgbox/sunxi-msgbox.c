5df8142e22be9b3431fd15bf96fc818f2ac5d70e
Samuel Holland <samuel@sholland.org>
2017-12-23 03:41:24 -0600

    drivers: sunxi-msgbox: Rewrite
    Signed-off-by: Samuel Holland <samuel@sholland.org>

--- drivers/msgbox/sunxi-msgbox.c
+++ drivers/msgbox/sunxi-msgbox.c
@@ -7,57 +7,87 @@
 #include <dm.h>
 #include <error.h>
 #include <mmio.h>
+#include <stddef.h>
 #include <util.h>
 #include <drivers/clock.h>
 #include <drivers/irqchip.h>
 #include <drivers/msgbox.h>
 
-#define MSGBOX_QUEUES             8
+/*
+ * The message box hardware provides 8 unidirectional channels. As the mailbox
+ * framework expects them to be bidirectional, create virtual channels out of
+ * pairs of opposite-direction hardware channels. The first channel in each
+ * pair
+ * is set up for AP->SCP communication, and the second channel is set up for
+ * SCP->AP transmission.
+ */
+#define NUM_CHANS               4
 
-#define MSGBOX_USER_ARISC         0
-#define MSGBOX_USER_ARM           1
-#define ME                        MSGBOX_USER_ARISC
+#define CTRL_REG0               (0x0000)
+#define CTRL_REG1               (0x0004)
 
-#define MSGBOX_CTRL_RX(n, u)      (BIT(8 * ((n) % 4)) * (u))
-#define MSGBOX_CTRL_TX(n, u)      (BIT(8 * ((n) % 4) + 4) * (u))
-#define MSGBOX_SENDER(n, u)       (MSGBOX_CTRL_RX(n, !(u)) | \
-	                           MSGBOX_CTRL_TX(n, u))
+/* These macros take a virtual channel number. */
+#define IRQ_EN_REG              (0x0040)
+#define IRQ_STATUS_REG          (0x0050)
+#define XMIT_IRQ(n)             BIT(3 + 4 * (n))
+#define RECV_IRQ(n)             BIT(0 + 4 * (n))
 
-#define MSGBOX_IRQ_BOTH(n)        BITMASK(2 * (n), 2)
-#define MSGBOX_IRQ_RX(n)          BIT(2 * (n))
-#define MSGBOX_IRQ_TX(n)          BIT(2 * (n) + 1)
+#define XMIT_FIFO_STATUS_REG(n) (0x0104 + 0x8 * (n))
+#define RECV_FIFO_STATUS_REG(n) (0x0100 + 0x8 * (n))
+#define FIFO_STATUS_MASK        BIT(0)
 
-#define MSGBOX_CTRL_REG0          (0x0000)
-#define MSGBOX_CTRL_REG1          (0x0004)
-#define MSGBOX_IRQ_EN_REG(u)      (0x0040 + 0x20 * (u))
-#define MSGBOX_IRQ_STATUS_REG(u)  (0x0050 + 0x20 * (u))
-#define MSGBOX_FIFO_STATUS_REG(n) (0x0100 + 0x04 * (n))
-#define MSGBOX_MSG_STATUS_REG(n)  (0x0140 + 0x04 * (n))
-#define MSGBOX_MSG_REG(n)         (0x0180 + 0x04 * (n))
+#define XMIT_MSG_STATUS_REG(n)  (0x0144 + 0x8 * (n))
+#define RECV_MSG_STATUS_REG(n)  (0x0140 + 0x8 * (n))
+#define MSG_STATUS_MASK         BITMASK(0, 3)
+
+#define XMIT_MSG_DATA_REG(n)    (0x0184 + 0x8 * (n))
+#define RECV_MSG_DATA_REG(n)    (0x0180 + 0x8 * (n))
+
+static msg_handler handlers[NUM_CHANS];
 
 static void
-sunxi_msgbox_handle_msg(struct device *dev, uint32_t queue)
+sunxi_msgbox_handle_msg(struct device *dev, uint32_t chan)
 {
-	info("msgbox received message %08x in queue %d",
-	     mmio_read32(dev->address + MSGBOX_MSG_REG(queue)), queue);
+	uint32_t msg = mmio_read32(dev->address + RECV_MSG_DATA_REG(chan));
+
+	if (handlers[chan])
+		handlers[chan](dev, chan, msg);
+	else
+		debug("unsolicited message %08x in channel %d", msg, chan);
 }
 
 static void
 sunxi_msgbox_irq(struct device *dev)
 {
-	uint32_t status =
-		mmio_read32(dev->address + MSGBOX_IRQ_STATUS_REG(ME));
+	uint32_t reg = mmio_read32(dev->address + IRQ_STATUS_REG);
 
-	for (uint32_t queue = 0; queue < MSGBOX_QUEUES; queue += 2) {
-		if (!(status & MSGBOX_IRQ_RX(queue)))
+	for (uint32_t chan = 0; chan < NUM_CHANS; ++chan) {
+		if (!(reg & RECV_IRQ(chan)))
 			continue;
-		while (mmio_read32(dev->address +
-		                   MSGBOX_MSG_STATUS_REG(queue)))
-			sunxi_msgbox_handle_msg(dev, queue);
+		while (mmio_read32(dev->address + RECV_MSG_STATUS_REG(chan)))
+			sunxi_msgbox_handle_msg(dev, chan);
 	}
 
 	/* Clear all processed pending interrupts. */
-	mmio_write32(dev->address + MSGBOX_IRQ_STATUS_REG(ME), status);
+	mmio_write32(dev->address + IRQ_STATUS_REG, reg);
+}
+
+static int
+sunxi_msgbox_register_handler(struct device *dev, uint8_t chan,
+                              msg_handler handler)
+{
+	uint32_t reg;
+
+	assert(chan < NUM_CHANS);
+
+	if (handlers[chan])
+		return EEXIST;
+	handlers[chan] = handler;
+
+	reg = mmio_read32(dev->address + IRQ_EN_REG);
+	mmio_write32(dev->address + IRQ_EN_REG, reg | RECV_IRQ(chan));
+
+	return 0;
 }
 
 static int
@@ -68,29 +98,9 @@ sunxi_msgbox_probe(struct device *dev)
 	if ((err = clock_enable(dev)))
 		return err;
 
-	/* Set queue directions. */
-	mmio_write32(dev->address + MSGBOX_CTRL_REG0,
-	             MSGBOX_SENDER(0, MSGBOX_USER_ARM) |
-	             MSGBOX_SENDER(1, MSGBOX_USER_ARISC) |
-	             MSGBOX_SENDER(2, MSGBOX_USER_ARM) |
-	             MSGBOX_SENDER(3, MSGBOX_USER_ARISC));
-	mmio_write32(dev->address + MSGBOX_CTRL_REG1,
-	             MSGBOX_SENDER(4, MSGBOX_USER_ARM) |
-	             MSGBOX_SENDER(5, MSGBOX_USER_ARISC) |
-	             MSGBOX_SENDER(6, MSGBOX_USER_ARM) |
-	             MSGBOX_SENDER(7, MSGBOX_USER_ARISC));
-
-	/* Clear and enable IRQs. */
-	mmio_write32(dev->address + MSGBOX_IRQ_STATUS_REG(ME),
-	             MSGBOX_IRQ_BOTH(0) | MSGBOX_IRQ_BOTH(1) | \
-	             MSGBOX_IRQ_BOTH(2) | MSGBOX_IRQ_BOTH(3) | \
-	             MSGBOX_IRQ_BOTH(4) | MSGBOX_IRQ_BOTH(5) | \
-	             MSGBOX_IRQ_BOTH(6) | MSGBOX_IRQ_BOTH(7));
-	mmio_write32(dev->address + MSGBOX_IRQ_EN_REG(ME),
-	             MSGBOX_IRQ_RX(0) | \
-	             MSGBOX_IRQ_RX(2) | \
-	             MSGBOX_IRQ_RX(4) | \
-	             MSGBOX_IRQ_RX(6));
+	/* Disable and clear all IRQs. */
+	mmio_write32(dev->address + IRQ_EN_REG, 0);
+	mmio_write32(dev->address + IRQ_STATUS_REG, BITMASK(0, 16));
 
 	if ((err = irqchip_register_irq(dev, sunxi_msgbox_irq)))
 		return err;
@@ -98,8 +108,34 @@ sunxi_msgbox_probe(struct device *dev)
 	return SUCCESS;
 }
 
+static int
+sunxi_msgbox_send_msg(struct device *dev, uint8_t chan, uint32_t msg)
+{
+	mmio_write32(dev->address + XMIT_MSG_DATA_REG(chan), msg);
+
+	return 0;
+}
+
+static int
+sunxi_msgbox_unregister_handler(struct device *dev, uint8_t chan)
+{
+	uint32_t reg;
+
+	assert(handlers[chan]);
+
+	reg = mmio_read32(dev->address + IRQ_EN_REG);
+	mmio_write32(dev->address + IRQ_EN_REG, reg & ~RECV_IRQ(chan));
+
+	handlers[chan] = NULL;
+
+	return 0;
+}
+
 static struct msgbox_driver_ops sunxi_msgbox_driver_ops = {
-	.class = DM_CLASS_MSGBOX,
+	.class              = DM_CLASS_MSGBOX,
+	.register_handler   = sunxi_msgbox_register_handler,
+	.send_msg           = sunxi_msgbox_send_msg,
+	.unregister_handler = sunxi_msgbox_unregister_handler,
 };
 
 struct driver sunxi_msgbox_driver = {
