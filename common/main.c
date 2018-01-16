8324be15e671daafa708ea7a281319a77eb5adef
drewwalters96 <drewwalters96@gmail.com>
2018-01-16 16:44:22 -0600

    common: Add main loop
    This commit adds a strategy for queueing and executing work items.
    
    Closes #14
    
    Signed-off-by: drewwalters96 <drewwalters96@gmail.com>

--- common/main.c
+++ common/main.c
@@ -1,10 +1,13 @@
 /*
  * Copyright © 2017 Samuel Holland <samuel@sholland.org>
+ * Copyright © 2017 Drew Walters <drewwalters96@gmail.com>
  * SPDX-License-Identifier: (BSD-3-Clause OR GPL-2.0)
  */
 
 #include <console.h>
 #include <dm.h>
+#include <stdbool.h>
+#include <work.h>
 #include <platform/devices.h>
 
 void main(void);
@@ -14,4 +17,10 @@ main(void)
 {
 	console_init(DEV_UART0);
 	dm_init();
+
+	while (true) {
+		process_work();
+
+		/* TODO: Enter sleep state */
+	}
 }
---
+++ include/work.h
@@ -0,0 +1,24 @@
+/*
+ * Copyright © 2017 Drew Walters <drewwalters96@gmail.com>
+ * SPDX-License-Identifier: (BSD-3-Clause OR GPL-2.0)
+ */
+
+#ifndef WORK_H
+#define WORK_H
+
+/**
+ * A function that performs asynchronous/delayed work.
+ */
+typedef void (*work_function)(void *);
+
+/**
+ * Execute each work item in the queue until it is empty.
+ */
+void process_work(void);
+
+/**
+ * Queue a work item, e.g. from an interrupt handler.
+ */
+void queue_work(work_function fn, void *param);
+
+#endif /* WORK_H */
