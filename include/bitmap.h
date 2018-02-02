c223f876124f0c07b6598e82f7027fefc2f700ef
Samuel Holland <samuel@sholland.org>
2018-02-02 13:32:16 -0600

    include: Add bitmap helper functions
    These type-safe helper functions are intended to replace the macro
    incantations duplicated at each usage site (which are hard to get right
    with regards to word/byte offsets and remembering to use BIT() around
    BITMAP_BIT()).
    
    Signed-off-by: Samuel Holland <samuel@sholland.org>

--- include/bitmap.h
+++ include/bitmap.h
@@ -7,10 +7,38 @@
 #define BITMAP_H
 
 #include <limits.h>
+#include <mmio.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <util.h>
 
 #define BITMAP_INDEX(word, bit) (WORD_BIT * (word) + (bit))
 #define BITMAP_WORD(index)      ((index) / WORD_BIT)
 #define BITMAP_WORDOFF(index)   (BITMAP_WORD(index) * (WORD_BIT / CHAR_BIT))
 #define BITMAP_BIT(index)       ((index) % WORD_BIT)
+
+static inline void
+bitmap_clear(uintptr_t base, uint32_t index)
+{
+	uintptr_t word = (uintptr_t)((uint32_t *)base + BITMAP_WORD(index));
+
+	mmio_clearbits32(word, BIT(BITMAP_BIT(index)));
+}
+
+static inline bool
+bitmap_get(uintptr_t base, uint32_t index)
+{
+	uintptr_t word = (uintptr_t)((uint32_t *)base + BITMAP_WORD(index));
+
+	return !!(mmio_read32(word) & BIT(BITMAP_BIT(index)));
+}
+
+static inline void
+bitmap_set(uintptr_t base, uint32_t index)
+{
+	uintptr_t word = (uintptr_t)((uint32_t *)base + BITMAP_WORD(index));
+
+	mmio_setbits32(word, BIT(BITMAP_BIT(index)));
+}
 
 #endif /* BITMAP_H */
