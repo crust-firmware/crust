2c31b38b9f80562a2b0a7b91a625cbfd619fbf48
Samuel Holland <samuel@sholland.org>
2018-02-02 15:03:18 -0600

    drivers: clock: sunxi-ccu: Start bitmap at register base
    Instead of adding the GATE/RESET offset to the register base, include it
    in the bitmap index. This removes the platform/device-specific
    dependency of the CCU driver that never should have been there.
    
    It's not completely clear what to use in the BASE offset divisor.
    (WORD_BIT / CHAR_BIT) would also work, and would be more akin to the
    other bitmap code. However, the helper functions assume uint32_t.
    
    Signed-off-by: Samuel Holland <samuel@sholland.org>

--- include/drivers/clock/sunxi-ccu.h
+++ include/drivers/clock/sunxi-ccu.h
@@ -6,12 +6,10 @@
 #ifndef DRIVERS_CLOCK_SUNXI_CCU_H
 #define DRIVERS_CLOCK_SUNXI_CCU_H
 
-#define CCU_GATE(n)       (((n) & 0xff) << 24)
-#define CCU_GET_GATE(n)   (((n) >> 24) & 0xff)
-#define CCU_RESET(n)      (((n) & 0xff) << 16)
-#define CCU_GET_RESET(n)  (((n) >> 16) & 0xff)
-#define CCU_MODULE(n)     ((n) & 0xffff)
-#define CCU_GET_MODULE(n) ((n) & 0xffff)
+#define CCU_GATE(n)      (((n) & 0xffff) << 16)
+#define CCU_GET_GATE(n)  (((n) >> 16) & 0xffff)
+#define CCU_RESET(n)     ((n) & 0xffff)
+#define CCU_GET_RESET(n) ((n) & 0xffff)
 
 extern const struct driver sunxi_ccu_driver;
 
