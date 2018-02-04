a92bdadca33d5137bb7e8cc8360164671c371b6f
Samuel Holland <samuel@sholland.org>
2018-02-03 19:34:13 -0600

    platform: sun50i: Add platform PRCM CCU definitions
    This is the CCU portion of the PRCM.
    
    The definitions for the RSB controller are omitted as the hardware is
    absent on the H5 SoC.
    
    Signed-off-by: Samuel Holland <samuel@sholland.org>

---
+++ platform/sun50i/include/platform/r_ccu.h
@@ -0,0 +1,28 @@
+/*
+ * Copyright © 2017-2018 The Crust Firmware Authors.
+ * SPDX-License-Identifier: (BSD-3-Clause OR GPL-2.0)
+ */
+
+#ifndef PLATFORM_R_CCU_H
+#define PLATFORM_R_CCU_H
+
+#include <bitmap.h>
+#include <stdint.h>
+
+#define R_CCU_GATE_BASE     (0x0028 / sizeof(uint32_t))
+
+#define R_CCU_GATE_R_PIO    BITMAP_INDEX(R_CCU_GATE_BASE + 0, 0)
+#define R_CCU_GATE_R_CIR    BITMAP_INDEX(R_CCU_GATE_BASE + 0, 1)
+#define R_CCU_GATE_R_TIMER  BITMAP_INDEX(R_CCU_GATE_BASE + 0, 2)
+#define R_CCU_GATE_R_UART   BITMAP_INDEX(R_CCU_GATE_BASE + 0, 4)
+#define R_CCU_GATE_R_I2C    BITMAP_INDEX(R_CCU_GATE_BASE + 0, 6)
+#define R_CCU_GATE_R_TWD    BITMAP_INDEX(R_CCU_GATE_BASE + 0, 7)
+
+#define R_CCU_RESET_BASE    (0x00b0 / sizeof(uint32_t))
+
+#define R_CCU_RESET_R_CIR   BITMAP_INDEX(R_CCU_RESET_BASE + 0, 1)
+#define R_CCU_RESET_R_TIMER BITMAP_INDEX(R_CCU_RESET_BASE + 0, 2)
+#define R_CCU_RESET_R_UART  BITMAP_INDEX(R_CCU_RESET_BASE + 0, 4)
+#define R_CCU_RESET_R_I2C   BITMAP_INDEX(R_CCU_RESET_BASE + 0, 6)
+
+#endif /* PLATFORM_R_CCU_H */
