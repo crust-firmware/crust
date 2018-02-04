08f4a8dbec3f1138970d89ebf38bff48ffc9908e
Samuel Holland <samuel@sholland.org>
2018-02-04 15:22:28 -0600

    include: Document bitmap header
    Signed-off-by: Samuel Holland <samuel@sholland.org>

--- include/bitmap.h
+++ include/bitmap.h
@@ -12,10 +12,35 @@
 #include <stdint.h>
 #include <util.h>
 
+/**
+ * Calculate a bit index from a (system word size) word offset and a bit offset
+ * within that word.
+ *
+ * @param word The offset of the word from the beginning of the bitmap.
+ * @param bit  The offset (arithmetic shift) of the bit within the word.
+ */
 #define BITMAP_INDEX(word, bit) (WORD_BIT * (word) + (bit))
+
+/**
+ * Extract the word offset from a bitmap index.
+ *
+ * @param index An index into a bitmap, in bits.
+ */
 #define BITMAP_WORD(index)      ((index) / WORD_BIT)
+
+/**
+ * Extract the bit offset (arithmetic shift) from a bitmap index.
+ *
+ * @param index An index into a bitmap, in bits.
+ */
 #define BITMAP_BIT(index)       ((index) % WORD_BIT)
 
+/**
+ * Clear a bit in a bitmap.
+ *
+ * @param base  The address of the start of the bitmap.
+ * @param index The index into the bitmap (in bits) of the bit to clear.
+ */
 static inline void
 bitmap_clear(uintptr_t base, uint32_t index)
 {
@@ -24,6 +49,12 @@ bitmap_clear(uintptr_t base, uint32_t index)
 	mmio_clearbits32(word, BIT(BITMAP_BIT(index)));
 }
 
+/**
+ * Get a bit in a bitmap.
+ *
+ * @param base  The address of the start of the bitmap.
+ * @param index The index into the bitmap (in bits) of the bit to get.
+ */
 static inline bool
 bitmap_get(uintptr_t base, uint32_t index)
 {
@@ -32,6 +63,12 @@ bitmap_get(uintptr_t base, uint32_t index)
 	return !!(mmio_read32(word) & BIT(BITMAP_BIT(index)));
 }
 
+/**
+ * Set a bit in a bitmap.
+ *
+ * @param base  The address of the start of the bitmap.
+ * @param index The index into the bitmap (in bits) of the bit to set.
+ */
 static inline void
 bitmap_set(uintptr_t base, uint32_t index)
 {
