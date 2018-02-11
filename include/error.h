crust-firmware/crust

commit 655525514e598946e12d721c26a27f982ad8b80a

include: Expand the list of possible errors
Add EINVAL and ERANGE. These will be used by new drivers.

Signed-off-by: Samuel Holland <samuel@sholland.org>

include/error.h
@@ -6,13 +6,15 @@
 #ifndef ERROR_H
 #define ERROR_H
 
-#define SUCCESS 0    /* The operation succeeded. */
-#define EAGAIN  (-1) /* Try again immediately. */
-#define EBUSY   (-2) /* Try again later. */
-#define EEXIST  (-3) /* The object already exists. */
-#define EIO     (-4) /* Communication with the hardware failed. */
-#define ENOBUS  (-5) /* The bus for this device does not exist. */
-#define ENODEV  (-6) /* This device does not exist. */
-#define ENOTSUP (-7) /* The operation is not supported. */
+#define SUCCESS 0     /* The operation succeeded. */
+#define EAGAIN  (-1)  /* Try again immediately. */
+#define EBUSY   (-2)  /* Try again later. */
+#define EEXIST  (-3)  /* The object already exists. */
+#define EINVAL  (-4)  /* The argument to the function was invalid. */
+#define EIO     (-5)  /* Communication with the hardware failed. */
+#define ENOBUS  (-6)  /* The bus for this device does not exist. */
+#define ENODEV  (-7)  /* This device does not exist. */
+#define ENOTSUP (-8)  /* The operation is not supported. */
+#define ERANGE  (-10) /* The argument to the function was out of range. */
 
 #endif /* ERROR_H */

commit 621aa891e58d816c77d52ddf17d4db25f0837fec

include: Add another error (EPERM) to the list
This fixes the inconsistency (number jump) introduced in 655525514e59,
because the new error name happens to be at the right place in the
alphabet to fill the hole.

While here, update the comments to be in the correct doxygen style.

Signed-off-by: Samuel Holland <samuel@sholland.org>

include/error.h
@@ -6,15 +6,16 @@
 #ifndef ERROR_H
 #define ERROR_H
 
-#define SUCCESS 0     /* The operation succeeded. */
-#define EAGAIN  (-1)  /* Try again immediately. */
-#define EBUSY   (-2)  /* Try again later. */
-#define EEXIST  (-3)  /* The object already exists. */
-#define EINVAL  (-4)  /* The argument to the function was invalid. */
-#define EIO     (-5)  /* Communication with the hardware failed. */
-#define ENOBUS  (-6)  /* The bus for this device does not exist. */
-#define ENODEV  (-7)  /* This device does not exist. */
-#define ENOTSUP (-8)  /* The operation is not supported. */
-#define ERANGE  (-10) /* The argument to the function was out of range. */
+#define SUCCESS 0     /**< The operation succeeded. */
+#define EAGAIN  (-1)  /**< Try again immediately. */
+#define EBUSY   (-2)  /**< Try again later. */
+#define EEXIST  (-3)  /**< The object already exists. */
+#define EINVAL  (-4)  /**< The argument to the function was invalid. */
+#define EIO     (-5)  /**< Communication with the hardware failed. */
+#define ENOBUS  (-6)  /**< The bus for this device does not exist. */
+#define ENODEV  (-7)  /**< The device does not exist. */
+#define ENOTSUP (-8)  /**< The operation is not supported. */
+#define EPERM   (-9)  /**< The operation is not permitted. */
+#define ERANGE  (-10) /**< The argument to the function was out of range. */
 
 #endif /* ERROR_H */