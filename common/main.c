6460d113e827ca6bf5de136af6861f934037c057
Samuel Holland <samuel@sholland.org>
2018-02-12 12:28:56 -0600

    common: Remove duplicate infinite loop after main
    Mark main() as noreturn so the compiler will warn if the function does
    not contain an infinite loop (i.e. it could possibly return).
    
    Signed-off-by: Samuel Holland <samuel@sholland.org>

--- common/main.c
+++ common/main.c
@@ -3,15 +3,16 @@
  * SPDX-License-Identifier: (BSD-3-Clause OR GPL-2.0)
  */
 
+#include <compiler.h>
 #include <console.h>
 #include <dm.h>
 #include <stdbool.h>
 #include <work.h>
 #include <platform/devices.h>
 
-void main(void);
+noreturn void main(void);
 
-void
+noreturn void
 main(void)
 {
 	console_init(DEV_UART0);
