94a4033a6b31fdd6a049c1e440953209d2d0c4bc
Samuel Holland <samuel@sholland.org>
2018-02-13 00:13:24 -0600

    platform: sun50i: Add R_TIMER0 device description
    This is the first of two channels in R_TIMER.
    
    Signed-off-by: Samuel Holland <samuel@sholland.org>

--- platform/sun50i/devices.c
+++ platform/sun50i/devices.c
@@ -9,19 +9,21 @@
 #include <drivers/irqchip/sun4i-intc.h>
 #include <drivers/msgbox/sunxi-msgbox.h>
 #include <drivers/pio/sunxi-pio.h>
+#include <drivers/timer/sun8i-r_timer.h>
 #include <drivers/wallclock/sun6i-cnt64.h>
 #include <platform/ccu.h>
 #include <platform/devices.h>
 #include <platform/irq.h>
 #include <platform/r_ccu.h>
 
-static struct device ccu     __device;
-static struct device msgbox  __device;
-static struct device pio     __device;
-static struct device r_ccu   __device;
-static struct device r_cnt64 __device;
-static struct device r_intc  __device;
-static struct device r_pio   __device;
+static struct device ccu      __device;
+static struct device msgbox   __device;
+static struct device pio      __device;
+static struct device r_ccu    __device;
+static struct device r_cnt64  __device;
+static struct device r_intc   __device;
+static struct device r_pio    __device;
+static struct device r_timer0 __device;
 
 static struct device ccu = {
 	.name = "ccu",
@@ -75,4 +77,16 @@ static struct device r_pio = {
 	.clockdev = &r_ccu,
 	.drv      = &sunxi_pio_driver,
 	.drvdata  = BIT(0), /*< Physically implemented ports (0). */
+};
+
+static struct device r_timer0 = {
+	.name  = "r_timer0",
+	.regs  = DEV_R_TIMER,
+	.clock = CCU_GATE(R_CCU_GATE_R_TIMER) |
+	         CCU_RESET(R_CCU_GATE_R_TIMER),
+	.clockdev = &r_ccu,
+	.drv      = &sun8i_r_timer_driver,
+	.drvdata  = 0, /*< Timer index within the device. */
+	.irq      = IRQ_R_TIMER0,
+	.irqdev   = &r_intc,
 };
