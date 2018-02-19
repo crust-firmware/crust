6b5a8cab911bd1314c5a1d6c5966682485b4f02a
Samuel Holland <samuel@sholland.org>
2018-02-18 20:54:13 -0600

    drivers: msgbox: Add a tx_pending msgbox driver hook
    This allows users of the message box framework to know when the messages
    they send have been received by the remote user.
    
    Signed-off-by: Samuel Holland <samuel@sholland.org>

--- include/drivers/msgbox.h
+++ include/drivers/msgbox.h
@@ -7,6 +7,7 @@
 #define DRIVERS_MSGBOX_H
 
 #include <dm.h>
+#include <stdbool.h>
 #include <stdint.h>
 
 #define MSGBOX_OPS(dev) ((struct msgbox_driver_ops *)((dev)->drv->ops))
@@ -14,10 +15,11 @@
 typedef void (*msgbox_handler)(struct device *dev, uint8_t chan, uint32_t msg);
 
 struct msgbox_driver_ops {
-	int (*register_handler)(struct device *dev, uint8_t chan,
-	                        msgbox_handler handler);
-	int (*send_msg)(struct device *dev, uint8_t chan, uint32_t msg);
-	int (*unregister_handler)(struct device *dev, uint8_t chan);
+	int  (*register_handler)(struct device *dev, uint8_t chan,
+	                         msgbox_handler handler);
+	int  (*send_msg)(struct device *dev, uint8_t chan, uint32_t msg);
+	bool (*tx_pending)(struct device *dev, uint8_t chan);
+	int  (*unregister_handler)(struct device *dev, uint8_t chan);
 };
 
 static inline int
@@ -31,6 +33,12 @@ static inline int
 msgbox_send_msg(struct device *dev, uint8_t chan, uint32_t msg)
 {
 	return MSGBOX_OPS(dev)->send_msg(dev, chan, msg);
+}
+
+static inline bool
+msgbox_tx_pending(struct device *dev, uint8_t chan)
+{
+	return MSGBOX_OPS(dev)->tx_pending(dev, chan);
 }
 
 static inline int
