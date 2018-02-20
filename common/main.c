e8d6634d40abcde3ae635ac4075a33a48cf6f986
Drew Walters <drewwalters96@gmail.com>
2018-02-20 16:52:45 -0600

    drivers: Enable watchdog
    This commit enables the trusted watchdog with a timeout of five seconds.
    
    Signed-off-by: Drew Walters <drewwalters96@gmail.com>

--- common/main.c
+++ common/main.c
@@ -5,19 +5,32 @@
 
 #include <compiler.h>
 #include <console.h>
+#include <debug.h>
 #include <dm.h>
 #include <stdbool.h>
 #include <work.h>
+#include <drivers/watchdog.h>
 #include <platform/devices.h>
+
+#define WDOG_TIMEOUT (5 * 1000 * 1000 * 24) /* 5 seconds */
 
 noreturn void main(void);
 
 noreturn void
 main(void)
 {
+	struct device *watchdog;
+
 	console_init(DEV_UART0);
 	dm_init();
 
+	/* Enable watchdog. */
+	if ((watchdog = dm_get_by_class(DM_CLASS_WATCHDOG))) {
+		watchdog_enable(watchdog, WDOG_TIMEOUT);
+		info("Trusted watchdog enabled");
+	}
+
+	/* Process work queue. */
 	while (true) {
 		process_work();
 
