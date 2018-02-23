7099a0a650c23c8e2d44270c5c74e6f55e0cdb51
Samuel Holland <samuel@sholland.org>
2018-02-22 21:28:21 -0600

    drivers: msgbox: Document the message box class API
    Signed-off-by: Samuel Holland <samuel@sholland.org>

--- include/drivers/msgbox.h
+++ include/drivers/msgbox.h
@@ -22,6 +22,14 @@ struct msgbox_driver_ops {
 	int  (*unregister_handler)(struct device *dev, uint8_t chan);
 };
 
+/**
+ * Register a handler to be called when messages are received on a message box
+ * channel.
+ *
+ * @param dev     The message box device.
+ * @param chan    The message box channel.
+ * @param handler The callback function to register.
+ */
 static inline int
 msgbox_register_handler(struct device *dev, uint8_t chan,
                         msgbox_handler handler)
@@ -29,18 +37,40 @@ msgbox_register_handler(struct device *dev, uint8_t chan,
 	return MSGBOX_OPS(dev)->register_handler(dev, chan, handler);
 }
 
+/**
+ * Send a message via a message box device.
+ *
+ * @param dev  The message box device.
+ * @param chan The channel to use within the message box.
+ * @param msg  The message to send.
+ */
 static inline int
 msgbox_send_msg(struct device *dev, uint8_t chan, uint32_t msg)
 {
 	return MSGBOX_OPS(dev)->send_msg(dev, chan, msg);
 }
 
+/**
+ * Check if a previous transmission on a message box channel is still pending.
+ * A message is pending until the reception IRQ has been cleared on the remote
+ * interface.
+ *
+ * @param dev  The message box device.
+ * @param chan    The message box channel.
+ */
 static inline bool
 msgbox_tx_pending(struct device *dev, uint8_t chan)
 {
 	return MSGBOX_OPS(dev)->tx_pending(dev, chan);
 }
 
+/**
+ * Unregister a message handler, so it will no longer be called for new
+ * messages on a message box channel.
+ *
+ * @param dev     The message box device.
+ * @param chan    The message box channel.
+ */
 static inline int
 msgbox_unregister_handler(struct device *dev, uint8_t chan)
 {
