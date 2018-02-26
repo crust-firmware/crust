27847a24b952fb2b155ca56fcd650d0b2974488b
Samuel Holland <samuel@sholland.org>
2018-02-25 20:10:15 -0600

    common: Provide a function for resetting the system
    This uses the watchdog to reset the system, or panics if that is not
    possible (which should also reset the system).
    
    Future functions provided by this module include system power-down and
    suspend.
    
    Signed-off-by: Samuel Holland <samuel@sholland.org>

---
+++ common/system_power.c
@@ -0,0 +1,24 @@
+/*
+ * Copyright © 2017-2018 The Crust Firmware Authors.
+ * SPDX-License-Identifier: (BSD-3-Clause OR GPL-2.0)
+ */
+
+#include <compiler.h>
+#include <debug.h>
+#include <delay.h>
+#include <dm.h>
+#include <stddef.h>
+#include <watchdog.h>
+
+noreturn void
+system_reset(void)
+{
+	struct device *watchdog = dm_get_by_class(DM_CLASS_WATCHDOG);
+
+	if (watchdog != NULL) {
+		watchdog_disable(watchdog);
+		watchdog_enable(watchdog, 0);
+		udelay(1);
+	}
+	panic("Failed to reset system");
+}
