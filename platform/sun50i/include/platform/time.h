e8874886087c0c3f4dd29b1c66ae720084478f3f
Samuel Holland <samuel@sholland.org>
2018-02-28 19:07:55 -0600

    platform: sun50i: Provide a header for time constants
    Provide the reference clock frequency as a platform-specific constant.
    
    Signed-off-by: Samuel Holland <samuel@sholland.org>

---
+++ platform/sun50i/include/platform/time.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright © 2017-2018 The Crust Firmware Authors.
+ * SPDX-License-Identifier: (BSD-3-Clause OR GPL-2.0)
+ */
+
+#ifndef PLATFORM_TIME_H
+#define PLATFORM_TIME_H
+
+#define REFCLK_MHZ 24
+#define REFCLK_KHZ (REFCLK_MHZ * 1000)
+#define REFCLK_HZ  (REFCLK_MHZ * 1000000)
+
+#endif /* PLATFORM_TIME_H */
