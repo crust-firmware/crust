25de5552e406bba64252caf36a51dc8d144cfb98
Samuel Holland <samuel@sholland.org>
2018-03-04 21:19:42 -0600

    common: Implement interrupt control functions
    These helper functions can be used to prevent critical sections from
    being preempted. No significant processing should be done inside a
    critical section, as it prevents handling of API calls and timer ticks.
    
    Signed-off-by: Samuel Holland <samuel@sholland.org>

---
+++ include/common/interrupts.h
@@ -0,0 +1,39 @@
+/*
+ * Copyright © 2017-2018 The Crust Firmware Authors.
+ * SPDX-License-Identifier: (BSD-3-Clause OR GPL-2.0)
+ */
+
+#ifndef COMMON_INTERRUPTS_H
+#define COMMON_INTERRUPTS_H
+
+#include <compiler.h>
+#include <spr.h>
+#include <stdint.h>
+
+/**
+ * Disable interrupts and provide a compiler barrier. The return value must be
+ * saved and passed to restore_interrupts() at the end of the critical section.
+ */
+static inline uint32_t __must_check
+disable_interrupts(void)
+{
+	uint32_t mask = SPR_SYS_SR_IEE_MASK | SPR_SYS_SR_TEE_MASK;
+	uint32_t reg  = mfspr(SPR_SYS_SR_ADDR);
+
+	mtspr(SPR_SYS_SR_ADDR, reg & ~mask);
+
+	return reg;
+}
+
+/**
+ * Restore interrupts and provide a compiler barrier.
+ *
+ * @param reg The register contents saved from a call to disable_interrupts().
+ */
+static inline void
+restore_interrupts(uint32_t reg)
+{
+	mtspr(SPR_SYS_SR_ADDR, reg);
+}
+
+#endif /* COMMON_INTERRUPTS_H */
