370aea0ded8c5b31ec511bd1a40354c4cc4cfbc1
Samuel Holland <samuel@sholland.org>
2018-03-06 18:36:27 -0600

    common: Remove the driver abstraction from the wall clock
    All sunxi SoCs that have the AR100 CPU also contain a 64-bit counter
    (sometimes called "CNT64" in the documentation) in the R_CPUCFG MMIO
    area. Instead of providing access to this device through a driver, put
    the function for reading it in common code. This allows it to be used
    at all times (including during driver model initialization) for delays
    and timeouts, and logging.
    
    In case future SoCs do not contain the same CNT64 hardware at the same
    offsets, declare the counter_read function as weak so it can be
    overridden in platform code (i.e. platform/newplatform/counter.c).
    
    Remove the old driver class, all existing drivers, and all references to
    the drivers in platform code.
    
    Signed-off-by: Samuel Holland <samuel@sholland.org>

---
+++ include/common/wallclock.h
@@ -0,0 +1,18 @@
+/*
+ * Copyright © 2017-2018 The Crust Firmware Authors.
+ * SPDX-License-Identifier: (BSD-3-Clause OR GPL-2.0)
+ */
+
+#ifndef COMMON_WALLCLOCK_H
+#define COMMON_WALLCLOCK_H
+
+#include <stdint.h>
+
+/**
+ * Read the platform's "wall clock", which is a monotonically-increasing 64-bit
+ * time stamp counter running at the platform's reference clock frequency. It
+ * starts counting from zero when the SoC is reset.
+ */
+uint64_t wallclock_read(void);
+
+#endif /* COMMON_WALLCLOCK_H */
