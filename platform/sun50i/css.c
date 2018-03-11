745919a51d603515480538a68dc95a76466ce0a8
Samuel Holland <samuel@sholland.org>
2018-03-11 01:54:58 -0600

    platform: sun50i: css: Implement cluster control
    These functions are developed with guidance from the algorithms given in
    the Cortex-A53 reference manual, Allwinner's published ATF source, and
    various pieces of SoC documentation.
    
    Signed-off-by: Samuel Holland <samuel@sholland.org>

--- platform/sun50i/css.c
+++ platform/sun50i/css.c
@@ -50,6 +50,9 @@ static const uint8_t power_switch_on_sequence[] = {
 	0xfe, 0xf8, 0xe0, 0x80, 0x00,
 };
 
+/* Reset Vector Base Address, one saved per cluster. */
+static uint32_t rvba[CLUSTER_MAX];
+
 /**
  * Spin until the bits in a register match an expected mask.
  *
@@ -109,6 +112,25 @@ css_get_cluster_count(void)
 }
 
 uint8_t
+css_get_cluster_state(uint8_t cluster)
+{
+	uint32_t reg;
+
+	assert(cluster < CLUSTER_MAX);
+
+	/* Are the cluster output clamps gated? */
+	reg = mmio_read32(DEV_R_PRCM + R_PRCM_PWROFF_GATING_REG(cluster));
+	if (reg & BIT(0))
+		return POWER_STATE_OFF;
+	/* Is the cluster in H_RESET? */
+	reg = mmio_read32(DEV_CPUCFG + CPUCFG_RESET_CTRL_REG(cluster));
+	if (!(reg & BIT(12)))
+		return POWER_STATE_OFF;
+
+	return POWER_STATE_ON;
+}
+
+uint8_t
 css_get_core_count(uint8_t cluster __unused)
 {
 	return CORE_MAX;
@@ -132,6 +154,87 @@ css_get_core_state(uint8_t cluster, uint8_t core)
 		return POWER_STATE_OFF;
 
 	return POWER_STATE_ON;
+}
+
+int
+css_set_cluster_state(uint8_t cluster, uint8_t state)
+{
+	uint8_t current_state = css_get_cluster_state(cluster);
+
+	assert(cluster < CLUSTER_MAX);
+
+	if (state == current_state)
+		return SUCCESS;
+
+	if (state == POWER_STATE_ON) {
+		/* Apply power to the cluster power domain. */
+		set_power_switch(cluster, 0, true);
+		/* Release the cluster output clamps. */
+		mmio_clearbits32(DEV_R_PRCM +
+		                 R_PRCM_PWROFF_GATING_REG(cluster), BIT(0));
+		udelay(1);
+		/* Deassert an undocumented reset bit (active-low). */
+		mmio_setbits32(DEV_R_CPUCFG + R_CPUCFG_SYS_RESET_REG, BIT(0));
+		/* Core 0 is enabled by default at power-on. Put it back in
+		 * reset and keep it there until the cluster is ready. */
+		mmio_clearbits32(DEV_CPUCFG +
+		                 CPUCFG_RESET_CTRL_REG(cluster), BIT(0));
+		/* Assert all cluster resets (active-low). */
+		mmio_write32(DEV_CPUCFG + CPUCFG_RESET_CTRL_REG(cluster), 0);
+		/* Enable hardware L2 cache flush (active-low). */
+		mmio_clearbits32(DEV_CPUCFG +
+		                 CPUCFG_CLUSTER_CTRL0_REG(cluster), BIT(4));
+		/* Put the cluster back into coherency (deassert ACINACTM). */
+		mmio_clearbits32(DEV_CPUCFG +
+		                 CPUCFG_CLUSTER_CTRL1_REG(cluster), BIT(0));
+		/*
+		 * Deassert all cluster resets (active-low). From MSB to LSB:
+		 *     Bit 28: AXI2MBUS interface reset
+		 *     Bit 24: SoC debug reset
+		 *     Bit 20: CPU MBIST reset
+		 *     Bit 12: Cluster H_RESET
+		 *     Bit  8: L2 cache reset
+		 */
+		mmio_write32(DEV_CPUCFG + CPUCFG_RESET_CTRL_REG(cluster),
+		             BIT(28) | BIT(24) | BIT(20) | BIT(12) | BIT(8));
+		/* Restore the reset vector base addresses for all cores. */
+		for (uint8_t core = 0; core < CORE_MAX; ++core) {
+			uintptr_t rvbar = CPUCFG_RVBAR_LO_REG(cluster, core);
+			mmio_write32(DEV_CPUCFG + rvbar, rvba[cluster]);
+		}
+	} else if (state == POWER_STATE_OFF) {
+		/* Wait for all CPUs to be idle. */
+		poll_bits(DEV_CPUCFG +
+		          CPUCFG_CPU_STATUS_REG(cluster), BITMASK(16, 4));
+		/* Save the power-on reset vector base address from core 0. */
+		rvba[cluster] = mmio_read32(DEV_CPUCFG +
+		                            CPUCFG_RVBAR_LO_REG(cluster, 0));
+		/* Assert L2FLUSHREQ to clean the cluster L2 cache. */
+		mmio_setbits32(DEV_CPUCFG + CPUCFG_GENERAL_CTRL_REG, BIT(8));
+		/* Wait for L2FLUSHDONE to go high. */
+		poll_bits(DEV_CPUCFG + CPUCFG_L2_STATUS_REG, BIT(10));
+		/* Deassert L2FLUSHREQ. */
+		mmio_clearbits32(DEV_CPUCFG + CPUCFG_GENERAL_CTRL_REG, BIT(8));
+		/* Remove the cluster from coherency (assert ACINACTM). */
+		mmio_setbits32(DEV_CPUCFG +
+		               CPUCFG_CLUSTER_CTRL1_REG(cluster), BIT(0));
+		/* Wait for the cluster (L2 cache) to be idle. */
+		poll_bits(DEV_CPUCFG + CPUCFG_CPU_STATUS_REG(cluster), BIT(0));
+		/* Assert all cluster resets (active-low). */
+		mmio_write32(DEV_CPUCFG + CPUCFG_RESET_CTRL_REG(cluster), 0);
+		/* Assert an undocumented reset bit (active-low). */
+		mmio_write32(DEV_R_CPUCFG + R_CPUCFG_SYS_RESET_REG, 0);
+		udelay(1);
+		/* Activate the cluster output clamps. */
+		mmio_setbits32(DEV_R_PRCM +
+		               R_PRCM_PWROFF_GATING_REG(cluster), BIT(0));
+		/* Remove power from the cluster power domain. */
+		set_power_switch(cluster, 0, false);
+	} else {
+		return EINVAL;
+	}
+
+	return SUCCESS;
 }
 
 int
