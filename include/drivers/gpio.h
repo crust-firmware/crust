c38d1dd36624ff2faa500bdfe1407aae65179afb
Drew Walters <drewwalters96@gmail.com>
2018-03-18 18:05:05 -0500

    drivers: Add GPIO pin structure
    This commit adds a GPIO pin structure for devices to keep track of
    the pins they utilize.
    
    Signed-off-by: Drew Walters <drewwalters96@gmail.com>

--- include/drivers/gpio.h
+++ include/drivers/gpio.h
@@ -14,6 +14,8 @@
 #define GPIO_OPS(dev) \
 	(&container_of((dev)->drv, struct gpio_driver, drv)->ops)
 
+#define GPIO_PINS(n) (struct gpio_handle[n])
+
 struct gpio_driver_ops {
 	int (*get_value)(struct device *dev, uint8_t pin, bool *value);
 	int (*set_mode)(struct device *dev, uint8_t pin, uint8_t mode);
@@ -23,6 +25,12 @@ struct gpio_driver_ops {
 struct gpio_driver {
 	const struct driver          drv;
 	const struct gpio_driver_ops ops;
+};
+
+struct gpio_handle {
+	struct device *const dev;
+	const uint8_t        pin;
+	const uint8_t        mode;
 };
 
 /**
