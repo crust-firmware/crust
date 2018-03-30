29ad49412c5d712ab45fa3c1cdd42ef40804eb06
Samuel Holland <samuel@sholland.org>
2018-03-30 17:18:11 -0500

    dm: Rework helper functions to allow device enumeration
    Signed-off-by: Samuel Holland <samuel@sholland.org>

--- include/common/dm.h
+++ include/common/dm.h
@@ -6,10 +6,15 @@
 #ifndef COMMON_DM_H
 #define COMMON_DM_H
 
+#include <stddef.h>
 #include <stdint.h>
 #include <util.h>
 
 #define __device __attribute__((section(".device"), used))
+
+#define for_each_dev_in_class(var, class) \
+	for (struct device *var = dm_first_dev_by_class(class); \
+	     var != NULL; var = dm_next_dev_by_class(class, var))
 
 enum {
 	DEVICE_FLAG_RUNNING = BIT(0),
@@ -77,21 +82,78 @@ struct driver {
 };
 
 /**
- * Get the first device of a given class.
+ * Get the total number of subdevices (channels, etc.) available in a class.
  *
  * @param class One of the enumerated driver classes.
+ * @return      The number of available subdevices.
  */
-struct device *dm_get_by_class(uint32_t class);
+uint8_t dm_count_subdevs_by_class(uint32_t class);
 
 /**
- * Get the device with the given name.
+ * Get the first device of a given class. This function only considers
+ * successfully-probed devices.
  *
- * @param name The name of the device.
+ * @param class One of the enumerated driver classes.
+ * @return      The address of the device description, or NULL if no
+ *              devices of this class were found.
  */
-struct device *dm_get_by_name(const char *name);
+struct device *dm_first_dev_by_class(uint32_t class);
+
+/**
+ * Get the next device of a given class. This function only considers
+ * successfully-probed devices.
+ *
+ * @param class One of the enumerated driver classes.
+ * @param prev	The previous device of this class, returned either by this
+ *              function or by {@code dm_first_dev_by_class}.
+ * @return      The address of the device description, or NULL if no more
+ *              devices of this class were found.
+ */
+struct device *dm_next_dev_by_class(uint32_t class, struct device *prev);
+
+/**
+ * Get the device with the given name. This function only considers
+ * successfully-probed devices.
+ *
+ * @param name  The name of the device.
+ * @return      The address of the device description, or NULL if no device
+ *              with this name was found.
+ */
+struct device *dm_get_dev_by_name(const char *name);
+
+/**
+ * Get the nth subdevice of a given class.
+ *
+ * If this function returns NULL, the value stored at {@code id} is undefined.
+ *
+ * @param class One of the enumerated driver classes.
+ * @param index The zero-based index of the subdevice within the class.
+ * @param id    An integer in which to store the per-device subdevice ID.
+ * @return      The controller device containing this subdevice, or NULL if
+ *              there is no subdevice with this index.
+ */
+struct device *dm_get_subdev_by_index(uint32_t class, uint8_t index,
+                                      uint8_t *id);
+
+/**
+ * Get the next subdevice of the same class as the given subdevice.
+ *
+ * If this function returns NULL, the value stored at {@code id} is undefined.
+ *
+ * @param dev   The controller device containing the previous subdevice.
+ * @param id    An integer containing the previous subdevice's per-device ID,
+ *              and in which to store the next subdevice's per-device ID.
+ * @return      The controller device containing this subdevice, or NULL if
+ *              there is no next subdevice.
+ */
+struct device *dm_next_subdev(struct device *dev, uint8_t *id);
 
 /**
  * Initialize the driver model, probing all devices in topological order.
+ *
+ * If a device cannot be probed because it is not present, a warning will be
+ * issued. If a device cannot be probed for any other reason, the firmware will
+ * panic.
  */
 void dm_init(void);
 
