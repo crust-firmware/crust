2c1b710bd1d5d4bbedb72efd36efb014617f0a5c
Samuel Holland <samuel@sholland.org>
2018-04-08 19:00:09 -0500

    drivers: Add a class for DVFS devices
    These are power domains that have discrete operating points associated
    with a clock frequency and a voltage. Unlike basic clocks and
    regulators, they are always enabled.
    
    Signed-off-by: Samuel Holland <samuel@sholland.org>

---
+++ include/drivers/dvfs.h
@@ -0,0 +1,90 @@
+/*
+ * Copyright © 2017-2018 The Crust Firmware Authors.
+ * SPDX-License-Identifier: (BSD-3-Clause OR GPL-2.0)
+ */
+
+#ifndef DRIVERS_DVFS_H
+#define DRIVERS_DVFS_H
+
+#include <dm.h>
+#include <intrusive.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <util.h>
+
+#define DVFS_OPS(dev) \
+	(&container_of((dev)->drv, struct dvfs_driver, drv)->ops)
+
+struct dvfs_opp {
+	const uint32_t rate;    /**< Clock rate in Hz. */
+	const uint16_t voltage; /**< Supply voltage in mV. */
+};
+
+struct dvfs_info {
+	const uint16_t         latency;   /**< Worst switching time in μs. */
+	const uint8_t          opp_count; /**< Number of operating points. */
+	const struct dvfs_opp *opps;      /**< Table of operating points. */
+};
+
+struct dvfs_driver_ops {
+	struct dvfs_info *(*get_info)(struct device *dev, uint8_t id);
+	uint8_t           (*get_opp)(struct device *dev, uint8_t id);
+	int               (*set_opp)(struct device *dev, uint8_t id,
+	                             uint8_t opp);
+};
+
+struct dvfs_driver {
+	const struct driver          drv;
+	const struct dvfs_driver_ops ops;
+};
+
+/**
+ * Get generic information about a DVFS domain.
+ *
+ * This function has no defined errors.
+ *
+ * @param dev The DVFS controller containing this domain.
+ * @param id  The device-specific identifier for this domain.
+ * @return    A pointer to the information structure.
+ */
+static inline struct dvfs_info *
+dvfs_get_info(struct device *dev, uint8_t id)
+{
+	return DVFS_OPS(dev)->get_info(dev, id);
+}
+
+/**
+ * Get the current power level of a DVFS domain, as an index into its operating
+ * point table.
+ *
+ * This function has no defined errors.
+ *
+ * @param dev The DVFS controller containing this domain.
+ * @param id  The device-specific identifier for this domain.
+ * @return    Zero on success; a defined error code on failure.
+ */
+static inline uint8_t
+dvfs_get_opp(struct device *dev, uint8_t id)
+{
+	return DVFS_OPS(dev)->get_opp(dev, id);
+}
+
+/**
+ * Set the current power level of a DVFS domain, given an index into its
+ * operating point table.
+ *
+ * This function may fail with:
+ *   EIO    There was a problem communicating with the hardware.
+ *   ERANGE The requested index is beyond the end of the operating point table.
+ *
+ * @param dev The DVFS controller containing this domain.
+ * @param id  The device-specific identifier for this domain.
+ * @param opp The location to store the operating point index.
+ */
+static inline int
+dvfs_set_opp(struct device *dev, uint8_t id, uint8_t opp)
+{
+	return DVFS_OPS(dev)->set_opp(dev, id, opp);
+}
+
+#endif /* DRIVERS_DVFS_H */
