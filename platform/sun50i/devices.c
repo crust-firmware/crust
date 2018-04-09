3675a23ca618fa583bd3bbb7e1b9822b84ea862d
Samuel Holland <samuel@sholland.org>
2018-04-08 19:54:49 -0500

    platform: sun50i: Add a CPUX device description
    Signed-off-by: Samuel Holland <samuel@sholland.org>

--- platform/sun50i/devices.c
+++ platform/sun50i/devices.c
@@ -8,6 +8,7 @@
 #include <i2c.h>
 #include <util.h>
 #include <clock/sunxi-ccu.h>
+#include <dvfs/cpux.h>
 #include <gpio/sunxi-gpio.h>
 #include <i2c/sun6i-a31-i2c.h>
 #include <irqchip/sun4i-intc.h>
@@ -31,6 +32,7 @@ static struct device axp803_pmic __device;
 static struct device axp803_regulator __device;
 #endif
 static struct device ccu      __device;
+static struct device cpux     __device;
 static struct device msgbox   __device;
 static struct device pio      __device;
 static struct device r_ccu    __device;
@@ -132,6 +134,19 @@ static struct device ccu = {
 		},
 	},
 	.subdev_count = CCU_CLOCK_COUNT,
+};
+
+static struct device cpux = {
+	.name = "cpux",
+	.regs = DEV_CCU,
+	.drv  = &cpux_driver.drv,
+#if CONFIG_REGULATOR_AXP803
+	.supplydev = &axp803_regulator,
+	.supply    = AXP803_REGL_DCDC2,
+#elif CONFIG_REGULATOR_SY8106A
+	.supplydev = &sy8106a,
+	.supply    = SY8106A_REGL_VOUT,
+#endif
 };
 
 static struct device msgbox = {
