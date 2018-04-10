2ecc2bea1e6619234bdfd9fde4713246a194b604
Drew Walters <drewwalters96@gmail.com>
2018-04-09 21:19:27 -0500

    drivers: pmic: Add axp803 pmic driver
    This commit adds a pmic driver for the axp803 device.
    
    Signed-off-by: Drew Walters <drewwalters96@gmail.com>

---
+++ include/drivers/pmic/axp803.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright © 2017-2018 The Crust Firmware Authors.
+ * SPDX-License-Identifier: (BSD-3-Clause OR GPL-2.0)
+ */
+
+#ifndef DRIVERS_PMIC_AXP803_H
+#define DRIVERS_PMIC_AXP803_H
+
+#include <pmic.h>
+
+extern const struct pmic_driver axp803_pmic_driver;
+
+#endif /* DRIVERS_PMIC_AXP803_H */
