19bbf6dce3341730810462e72f43521a71b70900
Adam Otto <agom94@mail.missouri.edu>
2018-04-09 20:37:07 -0500

    drivers: sensor: Add sun8i thermal sensor driver
    This is the thermal sensor found on recent Allwinner SoCs. For now, only
    the formulas for the A64 are included.
    
    Signed-off-by: Adam Otto <agom94@mail.missouri.edu>

---
+++ include/drivers/sensor/sun8i-thermal.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright © 2017-2018 The Crust Firmware Authors.
+ * SPDX-License-Identifier: (BSD-3-Clause OR GPL-2.0)
+ */
+
+#ifndef DRIVERS_SENSOR_SUN8I_THERMAL_H
+#define DRIVERS_SENSOR_SUN8I_THERMAL_H
+
+#include <sensor.h>
+
+extern const struct sensor_driver sun8i_thermal_driver;
+
+#endif /* DRIVERS_SENSOR_SUN8I_THERMAL_H */
