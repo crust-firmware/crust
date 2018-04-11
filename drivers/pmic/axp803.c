78c0a890d5afeb57d7b6270396e1a6a09df5ea97
Samuel Holland <samuel@sholland.org>
2018-04-11 01:18:52 -0500

    drivers: pmic: axp803: Add power button handling
    Now that the functions to handle power events exist, call them when the
    power button is pressed.
    
    Signed-off-by: Samuel Holland <samuel@sholland.org>

--- drivers/pmic/axp803.c
+++ drivers/pmic/axp803.c
@@ -7,19 +7,53 @@
 #include <dm.h>
 #include <error.h>
 #include <i2c.h>
+#include <irqchip.h>
 #include <pmic.h>
+#include <system_power.h>
 #include <mfd/axp803.h>
 #include <pmic/axp803.h>
 
 #define IRQ_ENABLE_REG1   0x40
+#define IRQ_ENABLE_REG5   0x44
 #define IRQ_ENABLE_REG6   0x45
 #define IRQ_STATUS_REG1   0x48
+#define IRQ_STATUS_REG5   0x4c
 #define IRQ_STATUS_REG6   0x4d
+
+#define GPIO0IRQ          BIT(0) /**< GPIO0 input edge. */
+#define GPIO1IRQ          BIT(1) /**< GPIO1 input edge. */
+#define POKOIRQ           BIT(2) /**< Power key power-off duration. */
+#define POKLIRQ           BIT(3) /**< Power key long duration. */
+#define POKSIRQ           BIT(4) /**< Power key short duration. */
+#define POKNIRQ           BIT(5) /**< Power key negative edge. */
+#define POKPIRQ           BIT(6) /**< Power key positive edge. */
+#define EVENTIRQ          BIT(7) /**< Event timeout. */
+#define WANTED_IRQS       (POKLIRQ | POKPIRQ)
 
 #define WAKEUP_CTRL_REG   0x31
 #define POWER_DISABLE_REG 0x32
 #define POK_CTRL_REG      0x59
 #define PIN_FUNCTION_REG  0x8f
+
+static void
+axp803_pmic_irq(void *param)
+{
+	struct device *dev = param;
+	int     err;
+	uint8_t reg;
+
+	/* IRQ register 5 is the only one with enabled IRQs. */
+	if ((err = i2c_read_reg(dev->bus, dev->addr, IRQ_STATUS_REG5, &reg)))
+		panic("%s: Cannot read NMI status: %d", dev->name, err);
+	if ((err = i2c_write_reg(dev->bus, dev->addr, IRQ_STATUS_REG5, reg)))
+		panic("%s: Cannot clear NMI 0x%02x: %d", dev->name, reg, err);
+
+	/* Reset on long press, otherwise wake the system. */
+	if (reg & POKLIRQ)
+		system_reset();
+	else
+		system_wakeup();
+}
 
 static int
 axp803_pmic_reset(struct device *dev)
@@ -65,9 +99,10 @@ axp803_pmic_probe(struct device *dev)
 	if ((err = i2c_write_reg(bus, addr, POK_CTRL_REG, BIT(4))))
 		return err;
 
-	/* Disable all unwanted IRQs. */
+	/* Set up IRQs; disable all but the relevant ones. */
 	for (uint8_t reg = IRQ_ENABLE_REG1; reg <= IRQ_ENABLE_REG6; ++reg) {
-		if ((err = i2c_write_reg(bus, addr, reg, 0)))
+		uint8_t data = reg == IRQ_ENABLE_REG5 ? WANTED_IRQS : 0;
+		if ((err = i2c_write_reg(bus, addr, reg, data)))
 			return err;
 	}
 
@@ -82,7 +117,8 @@ axp803_pmic_probe(struct device *dev)
 	if ((err = axp803_reg_setbits(dev, PIN_FUNCTION_REG, BITMASK(1, 3))))
 		return err;
 
-	return SUCCESS;
+	/* Register the NMI IRQ handler. */
+	return irqchip_enable(dev->irqdev, dev->irq, axp803_pmic_irq, dev);
 }
 
 const struct pmic_driver axp803_pmic_driver = {
