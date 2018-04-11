8c8605589a4cf2246690ba38ed20e0e58ec70e17
Drew Walters <drewwalters96@gmail.com>
2018-04-10 22:37:31 -0500

    drivers: sensor: Add temperature polling function
    This commit adds a function to periodically poll the temperature of
    all sensors and compare it to safe operating values.
    
    Signed-off-by: Drew Walters <drewwalters96@gmail.com>

--- drivers/sensor/sensor.c
+++ drivers/sensor/sensor.c
@@ -3,11 +3,15 @@
  * SPDX-License-Identifier: (BSD-3-Clause OR GPL-2.0)
  */
 
+#include <debug.h>
 #include <dm.h>
 #include <error.h>
 #include <sensor.h>
 #include <stdbool.h>
 #include <stdint.h>
+
+#define SENSOR_TEMP_MAX      100000
+#define SENSOR_TEMP_MIN_DVFS 90000
 
 int
 sensor_get_value(struct device *dev, uint8_t id, uint32_t *value)
@@ -24,4 +28,22 @@ sensor_get_value(struct device *dev, uint8_t id, uint32_t *value)
 	*value = info->offset + raw * info->multiplier;
 
 	return SUCCESS;
+}
+
+void
+sensor_poll_temp(void *param)
+{
+	struct device *dev = param;
+	uint32_t val       = UINT32_MAX;
+
+	/* Read sensor values and verify values do not pass trip points. */
+	for (size_t i = 0; i < dev->subdev_count; ++i) {
+		if (sensor_get_value(dev, i, &val))
+			error("Unable to read value of sensor with ID %u", i);
+
+		if (val > SENSOR_TEMP_MAX)
+			panic("Maximum, safe device temperature reached");
+		else if (val > SENSOR_TEMP_MIN_DVFS)
+			panic("Min DVFS not implemented.");
+	}
 }
