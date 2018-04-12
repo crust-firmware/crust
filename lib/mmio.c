03755cec6c28f86c3b45996abfdae03809b95cf1
Samuel Holland <samuel@sholland.org>
2018-04-11 19:39:34 -0500

    lib: Extract a helper for polling register bits
    Give it a more explanatory name that hints at it being an MMIO helper.
    
    Signed-off-by: Samuel Holland <samuel@sholland.org>

---
+++ lib/mmio.c
@@ -0,0 +1,15 @@
+/*
+ * Copyright © 2017-2018 The Crust Firmware Authors.
+ * SPDX-License-Identifier: (BSD-3-Clause OR GPL-2.0)
+ */
+
+#include <mmio.h>
+#include <stdint.h>
+
+void
+mmio_poll32(uintptr_t addr, uint32_t mask)
+{
+	while ((mmio_read32(addr) & mask) != mask) {
+		/* Wait for the bits to go high. */
+	}
+}
