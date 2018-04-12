c96b29b80e37e4feaf8507d76544bbd5f6ac813c
Samuel Holland <samuel@sholland.org>
2018-04-12 15:14:14 -0500

    drivers: pmic: Add a dummy PMIC driver
    This turns CPU voltage off in suspend/poweroff and turns it back on at
    resume/poweron/reset. While it saves some power, more importantly, it
    ensures the CPU is powered after reset from a "fake off" state.
    Otherwise, the SoC will not be able to properly reset.
    
    Signed-off-by: Samuel Holland <samuel@sholland.org>

---
+++ drivers/pmic/dummy.c
@@ -0,0 +1,47 @@
+/*
+ * Copyright © 2017-2018 The Crust Firmware Authors.
+ * SPDX-License-Identifier: (BSD-3-Clause OR GPL-2.0)
+ */
+
+#include <debug.h>
+#include <dm.h>
+#include <error.h>
+#include <pmic.h>
+#include <regulator.h>
+#include <pmic/dummy.h>
+
+static int
+dummy_pmic_power_off(struct device *dev)
+{
+	/* Turn CPU power off. */
+	return regulator_disable(dev->supplydev, dev->supply);
+}
+
+static int
+dummy_pmic_power_on(struct device *dev)
+{
+	/* Turn CPU power on. */
+	return regulator_enable(dev->supplydev, dev->supply);
+}
+
+static int
+dummy_pmic_probe(struct device *dev)
+{
+	if (dev->supplydev == NULL)
+		return ENODEV;
+
+	return SUCCESS;
+}
+
+const struct pmic_driver dummy_pmic_driver = {
+	.drv = {
+		.class = DM_CLASS_PMIC,
+		.probe = dummy_pmic_probe,
+	},
+	.ops = {
+		.reset    = dummy_pmic_power_on,
+		.shutdown = dummy_pmic_power_off,
+		.suspend  = dummy_pmic_power_off,
+		.wakeup   = dummy_pmic_power_on,
+	},
+};
