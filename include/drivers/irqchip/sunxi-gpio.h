08dca4a6e2f7dc0878e0d88cbcd56b28144abffc
Samuel Holland <samuel@sholland.org>
2018-04-12 21:30:25 -0500

    drivers: irqchip: Add sunxi-gpio irqchip driver
    This handles interrupts fed by external pins.
    
    Signed-off-by: Samuel Holland <samuel@sholland.org>

---
+++ include/drivers/irqchip/sunxi-gpio.h
@@ -0,0 +1,23 @@
+/*
+ * Copyright © 2017-2018 The Crust Firmware Authors.
+ * SPDX-License-Identifier: (BSD-3-Clause OR GPL-2.0)
+ */
+
+#ifndef DRIVERS_IRQCHIP_SUNXI_GPIO_H
+#define DRIVERS_IRQCHIP_SUNXI_GPIO_H
+
+#include <irqchip.h>
+
+#define SUNXI_GPIO_IRQ(port, index) (32 * (port) + (index))
+
+enum {
+	SUNXI_GPIO_IRQ_MODE_RISING_EDGE  = 0,
+	SUNXI_GPIO_IRQ_MODE_FALLING_EDGE = 1,
+	SUNXI_GPIO_IRQ_MODE_HIGH_LEVEL   = 2,
+	SUNXI_GPIO_IRQ_MODE_LOW_LEVEL    = 3,
+	SUNXI_GPIO_IRQ_MODE_EITHER_EDGE  = 4,
+};
+
+extern const struct irqchip_driver sunxi_gpio_irqchip_driver;
+
+#endif /* DRIVERS_IRQCHIP_SUNXI_GPIO_H */
