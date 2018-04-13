3661b9497426a010fceade679b34a8fcfa1fd807
Samuel Holland <samuel@sholland.org>
2018-04-12 21:57:19 -0500

    drivers: misc: Add a GPIO power button driver
    Signed-off-by: Samuel Holland <samuel@sholland.org>

---
+++ include/drivers/misc/gpio-button.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright © 2017-2018 The Crust Firmware Authors.
+ * SPDX-License-Identifier: (BSD-3-Clause OR GPL-2.0)
+ */
+
+#ifndef DRIVERS_MISC_GPIO_BUTTON_H
+#define DRIVERS_MISC_GPIO_BUTTON_H
+
+#include <dm.h>
+
+extern const struct driver gpio_button_driver;
+
+#endif /* DRIVERS_MISC_GPIO_BUTTON_H */
