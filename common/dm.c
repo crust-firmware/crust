crust-firmware/crust

commit c509b7c098cb6e02e5cbf56ef2f55c520c07fe12

common: dm: Clean up the device probe function
Use the helper function and perform some minor simplification.

Signed-off-by: Samuel Holland <samuel@sholland.org>

common/dm.c
@@ -32,7 +32,7 @@ device_probe(struct device *dev)
 	assert(dev->drv->probe);
 
 	/* Skip already-probed devices. */
-	if (dev->flags & DEVICE_FLAG_RUNNING)
+	if (device_is_running(dev))
 		return SUCCESS;
 	if (dev->flags & DEVICE_FLAG_MISSING)
 		return ENODEV;
@@ -45,16 +45,14 @@ device_probe(struct device *dev)
 	if (dev->supplydev && (err = device_probe(dev->supplydev)))
 		return err;
 
+	/* Initialize the device with the default number of subdevices. */
+	dev->subdev_count = 1;
 	/* Tell the device the index of its first subdevice. */
 	dev->subdev_index = total_subdevs[dev->drv->class];
 
 	/* Probe the device itself, and report any errors. */
 	if ((err = dev->drv->probe(dev)) == SUCCESS) {
 		dev->flags |= DEVICE_FLAG_RUNNING;
-		/* If the driver's probe function did not provide the number of
-		 * subdevices, assume the default number of 1. */
-		if (dev->subdev_count == 0)
-			dev->subdev_count = 1;
 		/* Update the total number of subdevices for this class. */
 		total_subdevs[dev->drv->class] += dev->subdev_count;
 		debug("dm: Probed %s", dev->name);

commit 0064cf11852c01647ede70b62c26c7d2cde04164

common: dm: Allow subdev count to be pre-initialized
Some drivers have a dynamic number of subdevices, based on the driver
data. To avoid extra pointers or flexible array members, the subdev
count is initialized in the device description. This was broken by
c509b7c0 "common: dm: Clean up the device probe function". Revert the
part that unconditionally set the subdevice count, and clarify in the
comment why the code is written this way.

This partially reverts commit c509b7c098cb6e02e5cbf56ef2f55c520c07fe12.

Signed-off-by: Samuel Holland <samuel@sholland.org>

common/dm.c
@@ -30,7 +30,7 @@ device_probe(struct device *dev)
 	int err;
 
 	/* Skip already-probed devices. */
-	if (device_is_running(dev))
+	if (dev->flags & DEVICE_FLAG_RUNNING)
 		return SUCCESS;
 	if (dev->flags & DEVICE_FLAG_MISSING)
 		return ENODEV;
@@ -41,14 +41,16 @@ device_probe(struct device *dev)
 	if (dev->supplydev && (err = device_probe(dev->supplydev)))
 		return err;
 
-	/* Initialize the device with the default number of subdevices. */
-	dev->subdev_count = 1;
 	/* Tell the device the index of its first subdevice. */
 	dev->subdev_index = total_subdevs[dev->drv->class];
 
 	/* Probe the device itself, and report any errors. */
 	if ((err = dev->drv->probe(dev)) == SUCCESS) {
 		dev->flags |= DEVICE_FLAG_RUNNING;
+		/* If the driver's probe function did not provide the number of
+		 * subdevices, assume the default number of 1. */
+		if (dev->subdev_count == 0)
+			dev->subdev_count = 1;
 		/* Update the total number of subdevices for this class. */
 		total_subdevs[dev->drv->class] += dev->subdev_count;
 		debug("dm: Probed %s", dev->name);