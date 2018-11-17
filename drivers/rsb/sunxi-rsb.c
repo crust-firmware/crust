3b053b3a898f7655144e4c610cc83581607d8a81
Samuel Holland <samuel@sholland.org>
2018-11-16 22:48:35 -0600

    drivers: rsb: Add sunxi RSB controller driver
    Use Kconfig to ensure that it only gets included for the A64 SoC, since
    it is not present on the H5.
    
    Signed-off-by: Samuel Holland <samuel@sholland.org>

---
+++ drivers/rsb/sunxi-rsb.c
@@ -0,0 +1,137 @@
+/*
+ * Copyright © 2017-2018 The Crust Firmware Authors.
+ * SPDX-License-Identifier: (BSD-3-Clause OR GPL-2.0)
+ */
+
+#include <clock.h>
+#include <dm.h>
+#include <error.h>
+#include <mmio.h>
+#include <rsb.h>
+#include <rsb/sunxi-rsb.h>
+
+#define RSB_CTRL_REG   0x00
+#define RSB_CCR_REG    0x04
+#define RSB_INT_EN_REG 0x08
+#define RSB_STAT_REG   0x0c
+#define RSB_ADDR_REG   0x10
+#define RSB_DLEN_REG   0x18
+#define RSB_DATA_REG   0x1c
+#define RSB_LCR_REG    0x24
+#define RSB_PMCR_REG   0x28
+#define RSB_CMD_REG    0x2c
+#define RSB_SADDR_REG  0x30
+
+#define I2C_BCAST_ADDR 0
+
+static int sunxi_rsb_set_rate(struct device *dev, uint32_t rate);
+
+static int
+sunxi_rsb_do_command(struct device *dev, uint32_t addr, uint32_t cmd)
+{
+	mmio_write32(dev->regs + RSB_CMD_REG, cmd);
+	mmio_write32(dev->regs + RSB_SADDR_REG, addr);
+	mmio_write32(dev->regs + RSB_CTRL_REG, BIT(7));
+
+	mmio_pollzero32(dev->regs + RSB_CTRL_REG, BIT(7));
+
+	return mmio_read32(dev->regs + RSB_STAT_REG) == BIT(0) ? SUCCESS : EIO;
+}
+
+static int
+sunxi_rsb_init_pmic(struct device *dev, uint32_t addr, uint8_t reg,
+                    uint8_t data)
+{
+	int err;
+
+	/* Switch the PMIC to RSB mode. */
+	mmio_write32(dev->regs + RSB_PMCR_REG,
+	             BIT(31) | data << 16 | reg << 8 | I2C_BCAST_ADDR);
+	mmio_pollzero32(dev->regs + RSB_PMCR_REG, BIT(31));
+
+	/* Raise the clock to 3MHz. */
+	if ((err = sunxi_rsb_set_rate(dev, 3000000)))
+		return err;
+
+	/* Set the PMIC's runtime address. */
+	return sunxi_rsb_do_command(dev, addr, RSB_SRTA);
+}
+
+static int
+sunxi_rsb_read(struct device *dev, uint8_t addr, uint8_t reg, uint8_t *data)
+{
+	int err;
+
+	mmio_write32(dev->regs + RSB_ADDR_REG, reg);
+
+	if ((err = sunxi_rsb_do_command(dev, RSB_RTADDR(addr), RSB_RD8)))
+		return err;
+
+	*data = mmio_read32(dev->regs + RSB_DATA_REG);
+
+	return SUCCESS;
+}
+
+static int
+sunxi_rsb_set_rate(struct device *dev, uint32_t rate)
+{
+	struct clock_handle *clock = &dev->clocks[0];
+	uint32_t dev_rate;
+	uint8_t  divider;
+	int err;
+
+	if ((err = clock_get_rate(clock->dev, clock->id, &dev_rate)))
+		return err;
+
+	dev_rate /= 2;
+	if (rate == 0 || rate < dev_rate / 256 || rate > dev_rate)
+		return ERANGE;
+
+	divider = dev_rate / rate - 1;
+	mmio_write32(dev->regs + RSB_CCR_REG, 1U << 8 | divider);
+
+	return SUCCESS;
+}
+
+static int
+sunxi_rsb_write(struct device *dev, uint8_t addr, uint8_t reg, uint8_t data)
+{
+	mmio_write32(dev->regs + RSB_ADDR_REG, reg);
+	mmio_write32(dev->regs + RSB_DATA_REG, data);
+
+	return sunxi_rsb_do_command(dev, RSB_RTADDR(addr), RSB_WR8);
+}
+
+static int
+sunxi_rsb_probe(struct device *dev)
+{
+	int err;
+
+	if ((err = dm_setup_clocks(dev, 1)))
+		return err;
+
+	if ((err = dm_setup_pins(dev, RSB_NUM_PINS)))
+		return err;
+
+	mmio_write32(dev->regs + RSB_CTRL_REG, BIT(0));
+	mmio_pollzero32(dev->regs + RSB_CTRL_REG, BIT(0));
+
+	/* Set the bus clock to a rate also compatible with I²C. */
+	if ((err = sunxi_rsb_set_rate(dev, 400000)))
+		return err;
+
+	return SUCCESS;
+}
+
+const struct rsb_driver sunxi_rsb_driver = {
+	.drv = {
+		.class = DM_CLASS_RSB,
+		.probe = sunxi_rsb_probe,
+	},
+	.ops = {
+		.init_pmic = sunxi_rsb_init_pmic,
+		.read      = sunxi_rsb_read,
+		.set_rate  = sunxi_rsb_set_rate,
+		.write     = sunxi_rsb_write,
+	},
+};
---
+++ include/drivers/rsb/sunxi-rsb.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright © 2017-2018 The Crust Firmware Authors.
+ * SPDX-License-Identifier: (BSD-3-Clause OR GPL-2.0)
+ */
+
+#ifndef DRIVERS_RSB_SUNXI_RSB_H
+#define DRIVERS_RSB_SUNXI_RSB_H
+
+#include <rsb.h>
+
+extern const struct rsb_driver sunxi_rsb_driver;
+
+#endif /* DRIVERS_RSB_SUNXI_RSB_H */
