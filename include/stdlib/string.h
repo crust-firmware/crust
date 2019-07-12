cda7106b9e072b297a52ba9ae899441d9a9367ca
Samuel Holland <samuel@sholland.org>
2019-07-11 23:44:35 -0500

    lib: Remove unused string functions
    Without SCPI commands for enumerating devices, nothing is passing
    strings around anymore. Remove some string functions that are
    no longer necessary.
    
    Signed-off-by: Samuel Holland <samuel@sholland.org>

--- include/stdlib/string.h
+++ include/stdlib/string.h
@@ -9,9 +9,6 @@
 #include <stddef.h>
 
 void *memcpy(void *restrict dest, const void *restrict src, size_t n);
-int strcmp(const char *a, const char *b);
 size_t strlen(const char *s);
-char *strncpy(char *restrict dest, const char *restrict src, size_t n);
-char *strncpy_swap(char *restrict dest, const char *restrict src, size_t n);
 
 #endif /* STDLIB_STRING_H */
--- lib/string.c
+++ lib/string.c
@@ -8,9 +8,7 @@
 #include <string.h>
 
 #undef memcpy
-#undef strcmp
 #undef strlen
-#undef strncpy
 
 void *
 memcpy(void *restrict dest, const void *restrict src, size_t n)
@@ -24,17 +22,6 @@ memcpy(void *restrict dest, const void *restrict src, size_t n)
 	return dest;
 }
 
-int
-strcmp(const char *a, const char *b)
-{
-	while (*a && *a == *b) {
-		++a;
-		++b;
-	}
-
-	return *a - *b;
-}
-
 size_t
 strlen(const char *s)
 {
@@ -44,32 +31,4 @@ strlen(const char *s)
 		++len;
 
 	return len;
-}
-
-char *
-strncpy(char *restrict dest, const char *restrict src, size_t n)
-{
-	char *d       = dest;
-	const char *s = src;
-
-	while (n-- > 0) {
-		*d++ = *s;
-		s   += *s != 0;
-	}
-
-	return dest;
-}
-
-char *
-strncpy_swap(char *restrict dest, const char *restrict src, size_t n)
-{
-	uintptr_t d   = (uintptr_t)dest;
-	const char *s = src;
-
-	while (n-- > 0) {
-		*(char *)(d++ ^ 3) = *s;
-		s += *s != 0;
-	}
-
-	return dest;
 }
