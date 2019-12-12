181dc3d74438ad4cd8a8b41cd85130a4d1cc294d
Samuel Holland <samuel@sholland.org>
2019-12-12 01:21:20 -0600

    dm: Remove regs member from struct device
    Now that all drivers that use MMIO have their own regs definition, the
    one in struct device can be removed to save space.
    
    Signed-off-by: Samuel Holland <samuel@sholland.org>

--- include/common/dm.h
+++ include/common/dm.h
@@ -20,8 +20,6 @@ struct driver;
 struct device {
 	/** A unique name for this device. */
 	const char *const          name;
-	/** The address of a block of MMIO registers (if one is used). */
-	const uintptr_t            regs;
 	/** The driver for this device. */
 	const struct driver *const drv;
 	/** Flags describing this device's state. */
