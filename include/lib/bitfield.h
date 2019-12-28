cadf1820ea91ba388bea88e572101560d49f0025
Samuel Holland <samuel@sholland.org>
2019-12-28 12:01:28 -0600

    lib: Add bitfield helpers for MMIO registers
    Signed-off-by: Samuel Holland <samuel@sholland.org>

--- include/lib/bitfield.h
+++ include/lib/bitfield.h
@@ -29,4 +29,25 @@ uint32_t bitfield_get(uint32_t word, uint32_t start, uint32_t width);
 uint32_t bitfield_set(uint32_t word, uint32_t start, uint32_t width,
                       uint32_t value);
 
+/**
+ * Get the value in a bitfield accessed via MMIO.
+ *
+ * @param addr  The address of a word containing a bitfield.
+ * @param start The offset of the starting bit (LSB) of the bitfield.
+ * @param width The width of the bitfield in bits.
+ */
+uint32_t mmio_get_bitfield_32(uintptr_t addr, uint32_t start, uint32_t width);
+
+/**
+ * Set the value in a bitfield accessed via MMIO.
+ *
+ * @param addr  The address of a word containing a bitfield.
+ * @param start The offset of the starting bit (LSB) of the bitfield.
+ * @param width The width of the bitfield in bits.
+ * @param value The value to place in the bitfield.
+ * @return      The original word, with the value of the bitfield replaced.
+ */
+void mmio_set_bitfield_32(uintptr_t addr, uint32_t start, uint32_t width,
+                          uint32_t value);
+
 #endif /* LIB_BITFIELD_H */
--- lib/bitfield.c
+++ lib/bitfield.c
@@ -4,6 +4,7 @@
  */
 
 #include <bitfield.h>
+#include <mmio.h>
 #include <stdint.h>
 #include <util.h>
 
@@ -17,4 +18,19 @@ uint32_t
 bitfield_set(uint32_t word, uint32_t start, uint32_t width, uint32_t value)
 {
 	return word ^ ((value << start ^ word) & ((BIT(width) - 1) << start));
+}
+
+uint32_t
+mmio_get_bitfield_32(uintptr_t addr, uint32_t start, uint32_t width)
+{
+	return bitfield_get(mmio_read_32(addr), start, width);
+}
+
+void
+mmio_set_bitfield_32(uintptr_t addr, uint32_t start, uint32_t width,
+                     uint32_t value)
+{
+	uint32_t word = mmio_read_32(addr);
+
+	mmio_write_32(addr, bitfield_set(word, start, width, value));
 }
