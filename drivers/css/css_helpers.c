1a054b60143c5fd766be0acc346835ccfabe462d
Samuel Holland <samuel@sholland.org>
2019-12-29 14:34:15 -0600

    drivers: css: Optimize css_set_power_switch
    Signed-off-by: Samuel Holland <samuel@sholland.org>

--- drivers/css/css_helpers.c
+++ drivers/css/css_helpers.c
@@ -11,10 +11,6 @@
 
 #include "css.h"
 
-static const uint8_t power_switch_off_sequence[] = {
-	0xff,
-};
-
 static const uint8_t power_switch_on_sequence[] = {
 	0xfe, 0xf8, 0xe0, 0x80, 0x00,
 };
@@ -22,26 +18,20 @@ static const uint8_t power_switch_on_sequence[] = {
 void
 css_set_power_switch(uintptr_t addr, bool enable)
 {
-	const uint8_t *values;
-	uint32_t length;
-
 	if (enable) {
-		values = power_switch_on_sequence;
-		length = ARRAY_SIZE(power_switch_on_sequence);
+		/* Avoid killing the power if the switch is already enabled. */
+		if (mmio_read_32(addr) == 0x00)
+			return;
+
+		/* Allwinner's blob uses 10, 20, and 30μs delays, depending on
+		 * the iteration. However, the same code works fine in ATF with
+		 * no delays. The 10μs delay is here just to be extra safe. */
+		const uint8_t *sequence = power_switch_on_sequence;
+		do {
+			mmio_write_32(addr, *sequence);
+			udelay(10);
+		} while (*sequence++ != 0x00);
 	} else {
-		values = power_switch_off_sequence;
-		length = ARRAY_SIZE(power_switch_off_sequence);
-	}
-
-	/* Avoid killing the power if the switch is already enabled. */
-	if (mmio_read_32(addr) == values[length - 1])
-		return;
-
-	/* Allwinner's blob uses 10, 20, and 30μs delays, depending on
-	 * the iteration. However, the same code works fine in ATF with
-	 * no delays. The 10μs delay is here just to be extra safe. */
-	for (uint32_t i = 0; i < length; ++i) {
-		mmio_write_32(addr, values[i]);
-		udelay(10);
+		mmio_write_32(addr, 0xff);
 	}
 }
