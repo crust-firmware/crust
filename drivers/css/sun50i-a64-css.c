crust-firmware/crust

commit 3a7b72427d99843171f0c67e573ff6072af6ed12

css: a64: Simplify state inspection functions
There is really no need to check two things for determining if a core or
cluster is off. It just wastes code size. Just pick a precondition that
is required for the cluster/core to function, ideally one guaranteed to
be exposed in the MMIO space.

Signed-off-by: Samuel Holland <samuel@sholland.org>

drivers/css/sun50i-a64-css.c
@@ -49,17 +49,14 @@ css_get_cluster_count(void)
 uint8_t
 css_get_cluster_state(uint8_t cluster)
 {
-	uint32_t reg;
-
 	assert(cluster < CLUSTER_MAX);
 
-	/* Are the cluster output clamps gated? */
-	reg = mmio_read_32(CLUSTER_PWROFF_GATING_REG);
-	if (reg & BIT(0))
-		return SCPI_CSS_OFF;
-	/* Is the cluster in H_RESET? */
-	reg = mmio_read_32(CLUSTER_RESET_CTRL_REG);
-	if (!(reg & BIT(12)))
+	/*
+	 * The cluster is considered off if it is not participating
+	 * in coherency (ACINACTM is set). This bit is chosen as
+	 * setting/clearing it is mandated by ARM.
+	 */
+	if (mmio_get_32(CLUSTER_CTRL_REG1, BIT(0)))
 		return SCPI_CSS_OFF;
 
 	return SCPI_CSS_ON;
@@ -74,18 +71,15 @@ css_get_core_count(uint8_t cluster UNUSED)
 uint8_t
 css_get_core_state(uint8_t cluster, uint8_t core)
 {
-	uint32_t reg;
-
 	assert(cluster < CLUSTER_MAX);
 	assert(core < CORE_MAX);
 
-	/* Is the core in power-on reset? */
-	reg = mmio_read_32(CLUSTER_PWRON_RESET_REG);
-	if (!(reg & BIT(core)))
-		return SCPI_CSS_OFF;
-	/* Is the core in core reset? */
-	reg = mmio_read_32(CLUSTER_RESET_CTRL_REG);
-	if (!(reg & BIT(core)))
+	/*
+	 * A core is considered off if it is in core reset. Regardless of any
+	 * deeper "off" state the core may or may not be in, if it is in
+	 * core reset, it is not running instructions or maintaining state.
+	 */
+	if (!mmio_get_32(CLUSTER_RESET_CTRL_REG, BIT(core)))
 		return SCPI_CSS_OFF;
 
 	return SCPI_CSS_ON;

commit 35ff2bf9ec3fd87e464f5e4838814189fc791955

css: Fix state inspection functions
ACINACTM is not a reliable indicator of the cluster's reset status. It
should be "1" when the cluster is in reset... except that the cluster is
turned off and not producing outputs. Let's use an input to the cluster
(one that has a stable meaning and location), not an output from it.

Fixes: 3a7b72427d9 ("css: a64: Simplify state inspection functions")
Signed-off-by: Samuel Holland <samuel@sholland.org>

drivers/css/sun50i-a64-css.c
@@ -52,11 +52,9 @@ css_get_cluster_state(uint8_t cluster)
 	assert(cluster < CLUSTER_MAX);
 
 	/*
-	 * The cluster is considered off if it is not participating
-	 * in coherency (ACINACTM is set). This bit is chosen as
-	 * setting/clearing it is mandated by ARM.
+	 * The cluster is considered off if its L2 cache is in reset.
 	 */
-	if (mmio_get_32(CLUSTER_CTRL_REG1, BIT(0)))
+	if (!mmio_get_32(CLUSTER_RESET_CTRL_REG, BIT(8)))
 		return SCPI_CSS_OFF;
 
 	return SCPI_CSS_ON;

drivers/css/sun50i-h6-css.c
@@ -51,11 +51,9 @@ css_get_cluster_state(uint8_t cluster)
 	assert(cluster < CLUSTER_MAX);
 
 	/*
-	 * The cluster is considered off if it is not participating
-	 * in coherency (ACINACTM is set). This bit is chosen as
-	 * setting/clearing it is mandated by ARM.
+	 * The cluster is considered off if its L2 cache is in reset.
 	 */
-	if (mmio_get_32(CLUSTER_CTRL_REG1, BIT(0)))
+	if (!mmio_get_32(CLUSTER_RESET_CTRL_REG, BIT(8)))
 		return SCPI_CSS_OFF;
 
 	return SCPI_CSS_ON;