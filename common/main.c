crust-firmware/crust

commit b50533d5cd4817ad0ce8f27408409261a096e412

common: Reset the firmware after an exception
Since all exceptions are fatal, there is no need to save any registers
when handling an exception. The exception number is already available in
`start`, so we can send it as a parameter to `main` instead of branching
early. And by printing the error message in the normal startup path, the
exception path can be removed entirely.

As an added bonus, instead of spinning and waiting for the watchdog to
reset the system, the firmware can immediately attempt to recover and
continue running.

Note that while .bss is wiped on a reset, .data is not set back to its
original values. This doesn't appear to be problematic at the moment:
devices aren't reinitialized, but they don't need to be. This will be
fully resolved in the future as all data is moved into .rodata or .bss.

Signed-off-by: Samuel Holland <samuel@sholland.org>

common/main.c
@@ -10,6 +10,7 @@
 #include <kconfig.h>
 #include <pmic.h>
 #include <scpi.h>
+#include <spr.h>
 #include <stdbool.h>
 #include <wallclock.h>
 #include <watchdog.h>
@@ -22,14 +23,19 @@
 
 #define WATCHDOG_TIMEOUT 5 /* seconds */
 
-noreturn void main(void);
+noreturn void main(uint32_t exception);
 
 noreturn void
-main(void)
+main(uint32_t exception)
 {
 	uint64_t next_tick = wallclock_read() + REFCLK_HZ;
 
 	console_init(DEV_UART0);
+
+	if (exception) {
+		error("Unhandled exception %u at %p!",
+		      exception, (void *)mfspr(SPR_SYS_EPCR_INDEX(0)));
+	}
 
 	/* Initialize and enable the watchdog first. This provides a failsafe
 	 * for the possibility that something below hangs. */

common/Makefile
@@ -9,12 +9,10 @@ obj-y += console.o
 obj-y += debug.o
 obj-y += delay.o
 obj-y += dm.o
-obj-y += exception.o
 obj-y += main.o
 obj-y += panic.o
 obj-y += scpi.o
 obj-y += scpi_cmds.o
 obj-y += start.o
 obj-y += system_power.o
-obj-y += vectors.o
 obj-y += wallclock.o

common/crust.ld.S
@@ -21,7 +21,7 @@ SECTIONS
   . = FIRMWARE_BASE;
 
   .text . : ALIGN(4) {
-    KEEP(*(.text.vector_stub_common))
+    KEEP(*(.text.start))
     *(SORT_BY_ALIGNMENT(SORT_BY_NAME(.text*)))
     . = ALIGN(4);
   } >SRAM_A2

common/exception.c
@@ -1,21 +1,0 @@
-/*
- * Copyright © 2017-2019 The Crust Firmware Authors.
- * SPDX-License-Identifier: BSD-3-Clause OR GPL-2.0-only
- */
-
-#include <debug.h>
-#include <exception.h>
-#include <spr.h>
-#include <stdint.h>
-
-void
-handle_exception(uint32_t number)
-{
-	switch (number) {
-	case TICK_TIMER_EXCEPTION:
-		break;
-	default:
-		panic("Unhandled exception %d at %p!",
-		      number, (void *)mfspr(SPR_SYS_EPCR_INDEX(0)));
-	}
-}

common/panic.S
@@ -9,6 +9,5 @@
 func panic
 	l.jal	log			# Log the panic message (clobbers r9)
 	l.nop
-1:	l.j	1b			# Infinite loop
-	l.nop
+	l.trap	0
 endfunc panic

common/start.S
@@ -7,11 +7,16 @@
 #include <arch/spr.h>
 
 func start
-	l.addi	r3, r0, 0		# Invalidate instruction cache
+	l.mfspr	r2, r0, SPR_SYS_PPC_ADDR
+	l.sfltui r2, 0x4000		# Size of exception vector area
+	l.bf	1f
+	l.srli	r2, r2, 8		# Vector address → exception number
+	l.addi	r2, r0, 0		# Set to zero if not an exception
+1:	l.addi	r3, r0, 0		# Invalidate instruction cache
 	l.addi	r4, r0, 4096		# Cache lines (256) * block size (16)
-1:	l.mtspr	r0, r3, SPR_ICACHE_ICBIR_ADDR
+2:	l.mtspr	r0, r3, SPR_ICACHE_ICBIR_ADDR
 	l.sfltu	r3, r4
-	l.bf	1b
+	l.bf	2b
 	l.addi	r3, r3, 16		# Cache block size
 	l.psync				# Flush CPU pipeline
 	l.mfspr	r3, r0, SPR_SYS_SR_ADDR	# Enable instruction cache
@@ -25,11 +30,12 @@ func start
 	l.ori	r3, r3, lo(__bss_start)
 	l.movhi	r4, hi(__bss_end)
 	l.ori	r4, r4, lo(__bss_end)
-1:	l.sw    0(r3), r0
-	l.sfltu r3, r4
-	l.bf	1b
+3:	l.sw	0(r3), r0
+	l.sfltu	r3, r4
+	l.bf	3b
 	l.addi	r3, r3, 4
 	l.movhi	r1, hi(__stack_end)
-	l.j	main			# Jump to C entry point
 	l.ori	r1, r1, lo(__stack_end)	# Initialize stack pointer
+	l.j	main			# Jump to C entry point
+	l.or	r3, r2, r2
 endfunc start

common/vectors.S
@@ -1,60 +1,0 @@
-/*
- * Copyright © 2017-2019 The Crust Firmware Authors.
- * SPDX-License-Identifier: BSD-3-Clause OR GPL-2.0-only
- */
-
-#include <macros.S>
-#include <arch/exception.h>
-#include <arch/spr.h>
-
-func vector_stub_common
-	l.mfspr	r2, r0, SPR_SYS_PPC_ADDR
-	l.srli	r2, r2, 8		# Exception number from vector address
-	l.sfgtui r2, TRAP_EXCEPTION
-	l.bf	start			# Invalid PPC means CPU reset
-	l.addi	r1, r1, -0x50		# 80 bytes for caller-saved registers
-	l.sw	0x00(r1), r3
-	l.sw	0x04(r1), r4
-	l.sw	0x08(r1), r5
-	l.sw	0x0c(r1), r6
-	l.sw	0x10(r1), r7
-	l.sw	0x14(r1), r8
-	l.sw	0x18(r1), r9
-	l.sw	0x1c(r1), r10
-	l.sw	0x20(r1), r11
-	l.sw	0x24(r1), r12
-	l.sw	0x28(r1), r13
-	l.sw	0x2c(r1), r15
-	l.sw	0x30(r1), r17
-	l.sw	0x34(r1), r19
-	l.sw	0x38(r1), r21
-	l.sw	0x3c(r1), r23
-	l.sw	0x40(r1), r25
-	l.sw	0x44(r1), r27
-	l.sw	0x48(r1), r29
-	l.sw	0x4c(r1), r31
-	l.jal	handle_exception
-	l.or	r3, r2, r2		# First arg is the exception number
-	l.lwz	r3,  0x00(r1)
-	l.lwz	r4,  0x04(r1)
-	l.lwz	r5,  0x08(r1)
-	l.lwz	r6,  0x0c(r1)
-	l.lwz	r7,  0x10(r1)
-	l.lwz	r8,  0x14(r1)
-	l.lwz	r9,  0x18(r1)
-	l.lwz	r10, 0x1c(r1)
-	l.lwz	r11, 0x20(r1)
-	l.lwz	r12, 0x24(r1)
-	l.lwz	r13, 0x28(r1)
-	l.lwz	r15, 0x2c(r1)
-	l.lwz	r17, 0x30(r1)
-	l.lwz	r19, 0x34(r1)
-	l.lwz	r21, 0x38(r1)
-	l.lwz	r23, 0x3c(r1)
-	l.lwz	r25, 0x40(r1)
-	l.lwz	r27, 0x44(r1)
-	l.lwz	r29, 0x48(r1)
-	l.lwz	r31, 0x4c(r1)
-	l.addi	r1, r1, 0x50
-	l.rfe				# No delay slot
-endfunc vector_stub_common

commit a5bcc53096eac908283a59fd90b562c5d5d8c957

main: Use the right macro for the EPCR SPR
SPR_SYS_EPCR_INDEX gets the index from the start of the SPR group, while
SPR_SYS_EPCR_ADDR gets the full address. They happen to be
interchangeable here because EPCR is in the first SPR group, but only
the semantic meaning of SPR_SYS_EPCR_ADDR is correct for the mtspr()
wrapper function, which expects the full zero-based address.

Fixes: b50533d5cd48 ("common: Reset the firmware after an exception")
Signed-off-by: Samuel Holland <samuel@sholland.org>

common/main.c
@@ -14,7 +14,7 @@ main(uint32_t exception)
 {
 	if (exception) {
 		error("Unhandled exception %u at %p!",
-		      exception, (void *)mfspr(SPR_SYS_EPCR_INDEX(0)));
+		      exception, (void *)mfspr(SPR_SYS_EPCR_ADDR(0)));
 	}
 
 	/* Cede control to the system state machine (never returns). */