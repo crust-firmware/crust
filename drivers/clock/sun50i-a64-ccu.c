d20a0da225b2cd6b176bee8f4ca8f791d86a2841
Samuel Holland <samuel@sholland.org>
2020-04-11 20:48:34 -0500

    drivers: sun50i-a64-ccu: Add CPUX, DRAM, and MBUS clocks
    Rate calculation for these clocks is omitted because it is not needed.
    
    The reset for CLK_BUS_DRAM is omitted because asserting it erases the
    DRAM controller configuration (i.e. training information).
    
    Signed-off-by: Samuel Holland <samuel@sholland.org>

--- drivers/clock/sun50i-a64-ccu.c
+++ drivers/clock/sun50i-a64-ccu.c
@@ -3,6 +3,7 @@
  * SPDX-License-Identifier: BSD-3-Clause OR GPL-2.0-only
  */
 
+#include <bitfield.h>
 #include <bitmap.h>
 #include <clock.h>
 #include <device.h>
@@ -20,16 +21,119 @@ sun50i_a64_ccu_fixed_get_rate(const struct ccu *self UNUSED,
 	return 600000000U;
 }
 
+static const struct clock_handle sun50i_a64_ccu_cpux_parents[] = {
+	{
+		.dev = &r_ccu.dev,
+		.id  = CLK_OSC32K,
+	},
+	{
+		.dev = &r_ccu.dev,
+		.id  = CLK_OSC24M,
+	},
+	{
+		.dev = &ccu.dev,
+		.id  = CLK_PLL_CPUX,
+	},
+	{
+		.dev = &ccu.dev,
+		.id  = CLK_PLL_CPUX,
+	},
+};
+
+static const struct clock_handle *
+sun50i_a64_ccu_cpux_get_parent(const struct ccu *self,
+                               const struct ccu_clock *clk)
+{
+	uint32_t val = mmio_read_32(self->regs + clk->reg);
+
+	return &sun50i_a64_ccu_cpux_parents[bitfield_get(val, 16, 2)];
+}
+
+static const struct clock_handle sun50i_a64_ccu_dram_parents[] = {
+	{
+		.dev = &ccu.dev,
+		.id  = CLK_PLL_DDR0,
+	},
+#if CONFIG_SOC_A64
+	{
+		.dev = &ccu.dev,
+		.id  = CLK_PLL_DDR1,
+	},
+#else
+	{
+		.dev = &ccu.dev,
+		.id  = CLK_PLL_PERIPH0,
+	},
+#endif
+};
+
+static const struct clock_handle *
+sun50i_a64_ccu_dram_get_parent(const struct ccu *self,
+                               const struct ccu_clock *clk)
+{
+	uint32_t val = mmio_read_32(self->regs + clk->reg);
+
+	return &sun50i_a64_ccu_dram_parents[bitfield_get(val, 20, 1)];
+}
+
 static const struct ccu_clock sun50i_a64_ccu_clocks[SUN50I_A64_CCU_CLOCKS] = {
+	[CLK_PLL_CPUX] = {
+		.get_parent = ccu_helper_get_parent,
+		.get_rate   = ccu_helper_get_rate,
+		.reg        = 0x0000,
+		.lock       = 28,
+		.gate       = BITMAP_INDEX(0x0000, 31),
+	},
+	[CLK_PLL_DDR0] = {
+		.get_parent = ccu_helper_get_parent,
+		.get_rate   = ccu_helper_get_rate,
+		.reg        = 0x0020,
+		.lock       = 28,
+		.update     = 20,
+		.gate       = BITMAP_INDEX(0x0020, 31),
+	},
 	[CLK_PLL_PERIPH0] = {
 		.get_parent = ccu_helper_get_parent,
 		.get_rate   = sun50i_a64_ccu_fixed_get_rate,
+	},
+#if CONFIG_SOC_A64
+	[CLK_PLL_DDR1] = {
+		.get_parent = ccu_helper_get_parent,
+		.get_rate   = ccu_helper_get_rate,
+		.reg        = 0x004c,
+		.lock       = 28,
+		.update     = 30,
+		.gate       = BITMAP_INDEX(0x004c, 31),
+	},
+#endif
+	[CLK_CPUX] = {
+		.get_parent = sun50i_a64_ccu_cpux_get_parent,
+		.get_rate   = ccu_helper_get_rate,
+		.reg        = 0x0050,
+	},
+	[CLK_BUS_DRAM] = {
+		.get_parent = ccu_helper_get_parent,
+		.get_rate   = ccu_helper_get_rate,
+		.gate       = BITMAP_INDEX(0x0060, 14),
 	},
 	[CLK_BUS_MSGBOX] = {
 		.get_parent = ccu_helper_get_parent,
 		.get_rate   = ccu_helper_get_rate,
 		.gate       = BITMAP_INDEX(0x0064, 21),
 		.reset      = BITMAP_INDEX(0x02c4, 21),
+	},
+	[CLK_DRAM] = {
+		.get_parent = sun50i_a64_ccu_dram_get_parent,
+		.get_rate   = ccu_helper_get_rate,
+		.reg        = 0x00f4,
+		.update     = 16,
+		.reset      = BITMAP_INDEX(0x00f4, 31),
+	},
+	[CLK_MBUS] = {
+		.get_parent = ccu_helper_get_parent,
+		.get_rate   = ccu_helper_get_rate,
+		.gate       = BITMAP_INDEX(0x015c, 31),
+		.reset      = BITMAP_INDEX(0x00fc, 31),
 	},
 };
 
