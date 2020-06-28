963c49d51f48cc030eac5f42f908a5e0ab314beb
Samuel Holland <samuel@sholland.org>
2020-06-28 15:01:44 -0500

    drivers: sun50i-h6-ccu: Reduce bus clock rates in suspend
    Signed-off-by: Samuel Holland <samuel@sholland.org>

--- drivers/clock/sun50i-h6-ccu.c
+++ drivers/clock/sun50i-h6-ccu.c
@@ -12,6 +12,32 @@
 
 #include "ccu.h"
 
+#define CPUX_AXI_CFG_REG  0x0500
+#define PSI_CFG_REG       0x0510
+#define AHB3_CFG_REG      0x051c
+#define APB1_CFG_REG      0x0520
+#define APB2_CFG_REG      0x0524
+
+#define CPUX_CLK_SRC(x)   ((x) << 24)
+#define CPUX_APB_CLK_M(x) ((x) << 8)
+#define CPUX_AXI_CLK_M(x) ((x) << 0)
+
+#define PSI_CLK_SRC(x)    ((x) << 24)
+#define PSI_CLK_P(x)      ((x) << 8)
+#define PSI_CLK_M(x)      ((x) << 0)
+
+#define AHB3_CLK_SRC(x)   ((x) << 24)
+#define AHB3_CLK_P(x)     ((x) << 8)
+#define AHB3_CLK_M(x)     ((x) << 0)
+
+#define APB1_CLK_SRC(x)   ((x) << 24)
+#define APB1_CLK_P(x)     ((x) << 8)
+#define APB1_CLK_M(x)     ((x) << 0)
+
+#define APB2_CLK_SRC(x)   ((x) << 24)
+#define APB2_CLK_P(x)     ((x) << 8)
+#define APB2_CLK_M(x)     ((x) << 0)
+
 static uint32_t
 sun50i_h6_ccu_fixed_get_rate(const struct ccu *self UNUSED,
                              const struct ccu_clock *clk UNUSED,
@@ -46,11 +72,57 @@ const struct ccu ccu = {
 void
 ccu_suspend(void)
 {
+	/* Set PSI/AHB1/AHB2 to LOSC/1 (32kHz). */
+	mmio_write_32(DEV_CCU + PSI_CFG_REG,
+	              PSI_CLK_SRC(1) |
+	              PSI_CLK_P(0) |
+	              PSI_CLK_M(0));
+
+	/* Set AHB3 to LOSC/1 (32kHz). */
+	mmio_write_32(DEV_CCU + AHB3_CFG_REG,
+	              AHB3_CLK_SRC(1) |
+	              AHB3_CLK_P(0) |
+	              AHB3_CLK_M(0));
+
+	/* Set APB1 to LOSC/2 (16kHz). */
+	mmio_write_32(DEV_CCU + APB1_CFG_REG,
+	              APB1_CLK_SRC(1) |
+	              APB1_CLK_P(1) |
+	              APB1_CLK_M(0));
 }
 
 void
 ccu_resume(void)
 {
+	/* Set CPUX to PLL_CPUX, APB to CPUX/4, AXI to CPUX/3. */
+	mmio_write_32(DEV_CCU + CPUX_AXI_CFG_REG,
+	              CPUX_CLK_SRC(3) |
+	              CPUX_APB_CLK_M(3) |
+	              CPUX_AXI_CLK_M(2));
+
+	/* Set PSI/AHB1/AHB2 to PLL_PERIPH0/3 (200MHz). */
+	mmio_write_32(DEV_CCU + PSI_CFG_REG,
+	              PSI_CLK_SRC(3) |
+	              PSI_CLK_P(0) |
+	              PSI_CLK_M(2));
+
+	/* Set AHB3 to PLL_PERIPH0/3 (200MHz). */
+	mmio_write_32(DEV_CCU + AHB3_CFG_REG,
+	              AHB3_CLK_SRC(3) |
+	              AHB3_CLK_P(0) |
+	              AHB3_CLK_M(2));
+
+	/* Set APB1 to PLL_PERIPH0/6 (100MHz). */
+	mmio_write_32(DEV_CCU + APB1_CFG_REG,
+	              APB1_CLK_SRC(3) |
+	              APB1_CLK_P(1) |
+	              APB1_CLK_M(2));
+
+	/* Set APB2 to OSC24M/1 (24MHz). */
+	mmio_write_32(DEV_CCU + APB2_CFG_REG,
+	              APB2_CLK_SRC(0) |
+	              APB2_CLK_P(0) |
+	              APB2_CLK_M(0));
 }
 
 void
