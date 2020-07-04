7764cf69102ea8e17e2d4ae060fcb2430962df7b
Samuel Holland <samuel@sholland.org>
2020-07-04 15:00:57 -0500

    drivers: serial: Add support for R_UART
    Signed-off-by: Samuel Holland <samuel@sholland.org>

--- drivers/serial/sun50i-h6-uart.c
+++ drivers/serial/sun50i-h6-uart.c
@@ -121,4 +121,32 @@ const struct simple_device uart = {
 	.regs = DEV_UART3,
 };
 
+#elif CONFIG(SERIAL_DEV_R_UART)
+
+const struct simple_device uart = {
+	.dev = {
+		.name  = "r_uart",
+		.drv   = &uart_driver,
+		.state = DEVICE_STATE_INIT,
+	},
+	.clock = { .dev = &r_ccu.dev, .id = CLK_BUS_R_UART },
+	.pins  = SIMPLE_DEVICE_PINS_INIT {
+		{
+			.dev   = &r_pio.dev,
+			.id    = SUNXI_GPIO_PIN(0, 2), /* PL2 */
+			.drive = DRIVE_10mA,
+			.mode  = 2,
+			.pull  = PULL_NONE,
+		},
+		{
+			.dev   = &r_pio.dev,
+			.id    = SUNXI_GPIO_PIN(0, 3), /* PL3 */
+			.drive = DRIVE_10mA,
+			.mode  = 2,
+			.pull  = PULL_UP,
+		},
+	},
+	.regs = DEV_R_UART,
+};
+
 #endif
