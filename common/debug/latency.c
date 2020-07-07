5ed4d1e3f79b3f134058ef0e97cb449aabf87dae
Samuel Holland <samuel@sholland.org>
2020-07-06 20:55:00 -0500

    debug: Use driver functions for counter access
    This replaces the open-coded SPR access in the latency measuring code.
    
    Signed-off-by: Samuel Holland <samuel@sholland.org>

--- common/debug/latency.c
+++ common/debug/latency.c
@@ -3,14 +3,14 @@
  * SPDX-License-Identifier: BSD-3-Clause OR GPL-2.0-only
  */
 
+#include <counter.h>
 #include <debug.h>
 #include <division.h>
-#include <spr.h>
 #include <system.h>
-#include <platform/time.h>
 
 #define ITERATIONS 10000
 
+static uint32_t cycles;
 static uint32_t iterations;
 static uint8_t  last_state;
 
@@ -20,14 +20,11 @@ debug_print_latency(void)
 	uint8_t current_state = get_system_state();
 
 	if (current_state != last_state) {
+		cycles     = counter_read();
 		iterations = 0;
 		last_state = current_state;
-		mtspr(SPR_TICK_TTMR_ADDR,
-		      SPR_TICK_TTMR_MODE_CONTINUE << SPR_TICK_TTMR_MODE_LSB);
-		mtspr(SPR_TICK_TTCR_ADDR, 0);
 	} else if (iterations < ITERATIONS && ++iterations == ITERATIONS) {
 		info("State %u: %u cycles/iteration", current_state,
-		     udiv_round(mfspr(SPR_TICK_TTCR_ADDR), ITERATIONS));
-		mtspr(SPR_TICK_TTMR_ADDR, 0);
+		     udiv_round(counter_read() - cycles, ITERATIONS));
 	}
 }
