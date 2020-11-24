bcadd7638f812e523af03a6f376f750fc046a66b
Samuel Holland <samuel@sholland.org>
2020-11-23 23:24:04 -0600

    drivers: css: Make most driver functions private
    Now that the CSS driver API only needs to provide a couple of functions,
    make the rest private. This makes the driver a more controlled
    environment, where it can make more assumptions about the various power
    domain states, and be more relaxed in caching them.
    
    Signed-off-by: Samuel Holland <samuel@sholland.org>

--- drivers/css/css.h
+++ drivers/css/css.h
@@ -10,6 +10,62 @@
 #include <stdint.h>
 
 /**
+ * Get the state of the compute subsystem (CSS).
+ */
+uint32_t css_get_css_state(void);
+
+/**
+ * Get the state of a cluster.
+ *
+ * @param cluster The index of the cluster.
+ */
+uint32_t css_get_cluster_state(uint32_t cluster);
+
+/**
+ * Get the number of cores present in a cluster.
+ *
+ * The number returned cannot be greater than 8.
+ *
+ * @param cluster The index of the cluster.
+ */
+uint32_t css_get_core_count(uint32_t cluster) ATTRIBUTE(const);
+
+/**
+ * Get the state of a CPU core.
+ *
+ * @param cluster The index of the cluster.
+ * @param core    The index of the core within the cluster.
+ */
+uint32_t css_get_core_state(uint32_t cluster, uint32_t core);
+
+/**
+ * Set the state of the compute subsystem (CSS). This state must not be
+ * numbered higher than the lowest cluster state in the CSS.
+ *
+ * @param state The coordinated requested state for the CSS.
+ */
+int css_set_css_state(uint32_t state);
+
+/**
+ * Set the state of a cluster. This state must not be numbered lower than the
+ * CSS state, nor higher than the lowest core state for this cluster.
+ *
+ * @param cluster The index of the cluster.
+ * @param state   The coordinated requested state for the cluster.
+ */
+int css_set_cluster_state(uint32_t cluster, uint32_t state);
+
+/**
+ * Set the state of a CPU core. This state must not be numbered lower than the
+ * core's cluster state.
+ *
+ * @param cluster The index of the cluster.
+ * @param core    The index of the core within the cluster.
+ * @param state   The coordinated requested state for the CPU core.
+ */
+int css_set_core_state(uint32_t cluster, uint32_t core, uint32_t state);
+
+/**
  * Enable or disable power to a core or cluster power domain.
  *
  * When enabling a power switch, the power domain will be turned on gradually
