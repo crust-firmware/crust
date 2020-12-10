83566e5c8a0a17e9960b586c0d050714ef043325
Samuel Holland <samuel@sholland.org>
2020-12-09 21:50:14 -0600

    drivers: css: Add an H3 CSS driver
    The H3 is a 32-bit chip, with Cortex-A7 CPUs instead of the Cortex-A53
    CPUs in the other supported platforms. It has a very different CPUCFG
    register layout from the 64-bit chips.
    
    For now, add just the part needed for CPU hotplug and shallow suspend.
    Full CPU subsystem suspend will be added later.
    
    Signed-off-by: Samuel Holland <samuel@sholland.org>

---
+++ drivers/css/sun8i-h3-css.c
@@ -0,0 +1,64 @@
+/*
+ * Copyright © 2017-2020 The Crust Firmware Authors.
+ * SPDX-License-Identifier: BSD-3-Clause OR GPL-2.0-only
+ */
+
+#include <mmio.h>
+#include <scpi_protocol.h>
+#include <stdint.h>
+#include <platform/cpucfg.h>
+#include <platform/prcm.h>
+
+#include "css.h"
+
+int
+css_set_css_state(uint32_t state UNUSED)
+{
+	/* Nothing to do. */
+	return SCPI_OK;
+}
+
+int
+css_set_cluster_state(uint32_t cluster UNUSED, uint32_t state UNUSED)
+{
+	/* Nothing to do. */
+	return SCPI_OK;
+}
+
+int
+css_set_core_state(uint32_t cluster UNUSED, uint32_t core, uint32_t state)
+{
+	if (state == SCPI_CSS_ON) {
+		/* Assert core reset and power-on reset (active-low). */
+		mmio_write_32(CPUn_RST_CTRL_REG(core), 0);
+		/* Enable hardware L1 cache flush (active-low). */
+		mmio_clr_32(GEN_CTRL_REG, GEN_CTRL_REG_L1RSTDISABLE(core));
+		/* Turn on power to the core power domain. */
+		css_set_power_switch(C0_CPUn_PWR_SWITCH_REG(core), true);
+		/* Release the core output clamps. */
+		mmio_clr_32(C0_PWROFF_GATING_REG, C0_CPUn_PWROFF_GATING(core));
+		/* Deassert core reset and power-on reset (active-low). */
+		mmio_write_32(CPUn_RST_CTRL_REG(core),
+		              CPUn_RST_CTRL_REG_nCORERESET |
+		              CPUn_RST_CTRL_REG_nCPUPORESET);
+		/* Assert DBGPWRDUP (allow debug access to the core). */
+		mmio_set_32(DBG_CTRL_REG1, DBG_CTRL_REG1_DBGPWRDUP(core));
+	} else if (state == SCPI_CSS_OFF) {
+		/* Wait for the core to be in WFI and ready to shut down. */
+		mmio_poll_32(CPUn_STATUS_REG(core),
+		             CPUn_STATUS_REG_STANDBYWFI);
+		/* Deassert DBGPWRDUP (prevent debug access to the core). */
+		mmio_clr_32(DBG_CTRL_REG1, DBG_CTRL_REG1_DBGPWRDUP(core));
+		/* Assert core reset and power-on reset (active-low). */
+		mmio_write_32(CPUn_RST_CTRL_REG(core), 0);
+		/* Activate the core output clamps. */
+		mmio_set_32(C0_PWROFF_GATING_REG, C0_CPUn_PWROFF_GATING(core));
+		/* Remove power from the core power domain. */
+		css_set_power_switch(C0_CPUn_PWR_SWITCH_REG(core), false);
+	} else {
+		/* Unknown power state requested. */
+		return SCPI_E_PARAM;
+	}
+
+	return SCPI_OK;
+}
