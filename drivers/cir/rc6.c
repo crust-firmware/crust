16c4fde92659f13c4db67131266d8ef51e80c938
Samuel Holland <samuel@sholland.org>
2020-12-20 00:35:06 -0600

    drivers: cir: Add an RC6 protocol decoder
    Use a basic state machine optimized for code size. All processing is
    done in units of the sample clock, and the exact durations are provided
    by the calling driver to avoid multiplication at runtime. The decoder
    restarts upon encountering any error.
    
    This decoder was written with the sunxi-cir hardware in mind, and that
    is reflected in the data types in the context structure. It assumes a
    relatively low sample rate, so even the longest pulses only result in a
    single sample from the hardware.
    
    Currently, only the "RC6-6A-32" variant is implemented. The trailer
    (mode 0 toggle bit) is completely ignored. The decoder expects 32 data
    bits and masks off the toggle bit from the data word.
    
    Signed-off-by: Samuel Holland <samuel@sholland.org>

---
+++ drivers/cir/rc6.c
@@ -0,0 +1,94 @@
+/*
+ * Copyright © 2020 The Crust Firmware Authors.
+ * SPDX-License-Identifier: BSD-3-Clause OR GPL-2.0-only
+ */
+
+#include <debug.h>
+#include <stdint.h>
+#include <util.h>
+
+#include "rc6.h"
+
+#define NUM_DATA_BITS   32
+#define NUM_HEADER_BITS 4
+
+uint32_t
+rc6_decode(struct rc6_ctx *ctx)
+{
+	int32_t duration = ctx->durations[ctx->state];
+	int32_t epsilon  = duration >> 1;
+
+	/* Subtract the expected pulse with from the sample width. */
+	ctx->width -= duration;
+
+	/*
+	 * If the duration of this pulse is larger than the remaining time in
+	 * the current sample, an error has occurred. Either noise introduced
+	 * a short pulse, or the decoder got out of sync. Restart the decoder.
+	 */
+	if (ctx->width < -epsilon) {
+		if (ctx->state != RC6_IDLE)
+			debug("RC6 fail %x", ctx->state);
+		ctx->state = RC6_IDLE;
+		return 0;
+	}
+
+	/* Discard the remainder of the sample if less than epsilon remains. */
+	if (ctx->width <= epsilon)
+		ctx->width = 0;
+
+	switch (ctx->state) {
+	case RC6_IDLE:
+		if (!ctx->pulse)
+			break;
+		/* Found a leader mark. Initialize the context. */
+		ctx->bits   = 0;
+		ctx->buffer = 0;
+		ctx->state  = RC6_LEADER_S;
+		break;
+	case RC6_LEADER_S:
+		/* Expect a space. */
+		ctx->state = ctx->pulse ? RC6_IDLE : RC6_HEADER_P;
+		break;
+	case RC6_HEADER_P:
+	case RC6_DATA_P:
+		ctx->bits++;
+		ctx->buffer = ctx->buffer << 1 | ctx->pulse;
+		ctx->state++;
+		break;
+	case RC6_HEADER_N:
+		/* This pulse must negate the previous pulse. */
+		if (ctx->pulse == (ctx->buffer & 1)) {
+			ctx->state = RC6_IDLE;
+		} else if (ctx->bits == NUM_HEADER_BITS) {
+			/* Reinitialize the buffer for decoding data. */
+			ctx->bits   = 0;
+			ctx->buffer = 0;
+			ctx->state  = RC6_TRAILER_P;
+		} else {
+			ctx->state = RC6_HEADER_P;
+		}
+		break;
+	case RC6_TRAILER_P:
+	case RC6_TRAILER_N:
+		ctx->state++;
+		break;
+	case RC6_DATA_N:
+		/* This pulse must negate the previous pulse. */
+		if (ctx->pulse == (ctx->buffer & 1)) {
+			ctx->state = RC6_IDLE;
+		} else if (ctx->bits == NUM_DATA_BITS) {
+			ctx->state = RC6_IDLE;
+			/* Return the scan code minus the toggle bit. */
+			debug("RC6 code %08x", ctx->buffer);
+			return ctx->buffer & ~BIT(15);
+		} else {
+			ctx->state = RC6_DATA_P;
+		}
+		break;
+	default:
+		unreachable();
+	}
+
+	return 0;
+}
---
+++ drivers/cir/rc6.h
@@ -0,0 +1,45 @@
+/*
+ * Copyright © 2020 The Crust Firmware Authors.
+ * SPDX-License-Identifier: BSD-3-Clause OR GPL-2.0-only
+ */
+
+#ifndef RC6_PRIVATE_H
+#define RC6_PRIVATE_H
+
+#include <stdint.h>
+
+enum {
+	RC6_IDLE,
+	RC6_LEADER_S,
+	RC6_HEADER_P,
+	RC6_HEADER_N,
+	RC6_TRAILER_P,
+	RC6_TRAILER_N,
+	RC6_DATA_P,
+	RC6_DATA_N,
+	RC6_STATES
+};
+
+struct rc6_ctx {
+	const int8_t *durations;
+	uint32_t      buffer;
+	uint8_t       bits;
+	uint8_t       state;
+	uint8_t       pulse;
+	int8_t        width;
+};
+
+/**
+ * Decode an RC6 pulse sequence.
+ *
+ * The pulse flag and width must be valid each time this function is called.
+ * Each call will decode a single pulse, so decoding a complete scancode
+ * requires many calls. All but the last in the sequence will return zero.
+ *
+ * If an error occurs, decoding restarts, but the error is not reported.
+ *
+ * @return A successfully decoded scancode, or zero.
+ */
+uint32_t rc6_decode(struct rc6_ctx *ctx);
+
+#endif /* RC6_PRIVATE_H */
