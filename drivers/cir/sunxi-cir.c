5d69a1108312cf1afcd8b4c227e8b3ba82e6ab5e
Samuel Holland <samuel@sholland.org>
2020-12-20 00:38:48 -0600

    drivers: sunxi-cir: Configure for 32 kHz sample clock
    This removes the restriction on disabling OSC24M. While directly
    manipulating the PRCM is ugly, I am not interested in adding
    rate-setting capability back to the clock drivers unless absolutely
    necessary. In this case, the clock register format is common across all
    supported platforms, so it works out.
    
    Signed-off-by: Samuel Holland <samuel@sholland.org>

--- drivers/cir/sunxi-cir.c
+++ drivers/cir/sunxi-cir.c
@@ -10,6 +10,7 @@
 #include <clock/ccu.h>
 #include <gpio/sunxi-gpio.h>
 #include <platform/devices.h>
+#include <platform/prcm.h>
 
 #define CIR_RXCTL  0x00
 #define CIR_RXPCFG 0x10
@@ -18,10 +19,23 @@
 #define CIR_RXSTA  0x30
 #define CIR_RXCFG  0x34
 
+struct sunxi_cir_state {
+	struct device_state ds;
+	uint32_t            clk_stash;
+	uint32_t            cfg_stash;
+	uint32_t            ctl_stash;
+};
+
 static inline const struct sunxi_cir *
 to_sunxi_cir(const struct device *dev)
 {
 	return container_of(dev, const struct sunxi_cir, dev);
+}
+
+static inline struct sunxi_cir_state *
+sunxi_cir_state_for(const struct device *dev)
+{
+	return container_of(dev->state, struct sunxi_cir_state, ds);
 }
 
 uint32_t
@@ -35,12 +49,32 @@ sunxi_cir_poll(const struct device *dev)
 static int
 sunxi_cir_probe(const struct device *dev UNUSED)
 {
+	const struct sunxi_cir *self  = to_sunxi_cir(dev);
+	struct sunxi_cir_state *state = sunxi_cir_state_for(dev);
+
+	state->clk_stash = mmio_read_32(R_CIR_RX_CLK_REG);
+	mmio_write_32(R_CIR_RX_CLK_REG, 0x80000000);
+
+	state->cfg_stash = mmio_read_32(self->regs + CIR_RXCFG);
+	mmio_write_32(self->regs + CIR_RXCFG, 0x010f0310);
+
+	state->ctl_stash = mmio_read_32(self->regs + CIR_RXCTL);
+	mmio_write_32(self->regs + CIR_RXCTL, 0x30);
+	mmio_write_32(self->regs + CIR_RXCTL, 0x33);
+
 	return SUCCESS;
 }
 
 static void
 sunxi_cir_release(const struct device *dev UNUSED)
 {
+	const struct sunxi_cir *self  = to_sunxi_cir(dev);
+	struct sunxi_cir_state *state = sunxi_cir_state_for(dev);
+
+	mmio_write_32(R_CIR_RX_CLK_REG, state->clk_stash);
+	mmio_write_32(self->regs + CIR_RXCFG, state->cfg_stash);
+	mmio_write_32(self->regs + CIR_RXCTL, 0x30);
+	mmio_write_32(self->regs + CIR_RXCTL, state->ctl_stash);
 }
 
 static const struct driver sunxi_cir_driver = {
@@ -52,7 +86,7 @@ const struct sunxi_cir r_cir_rx = {
 	.dev = {
 		.name  = "r_cir_rx",
 		.drv   = &sunxi_cir_driver,
-		.state = DEVICE_STATE_INIT,
+		.state = &(struct sunxi_cir_state) { { 0 } }.ds,
 	},
 	.bus_clock = { .dev = &r_ccu.dev, .id = CLK_BUS_R_CIR },
 	.mod_clock = { .dev = &r_ccu.dev, .id = CLK_R_CIR },
