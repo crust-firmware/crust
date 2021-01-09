cd6ddc01c21bc02785b86a4094949a31db1cd36f
Samuel Holland <samuel@sholland.org>
2021-01-08 23:40:32 -0600

    common: Remove duplication in regulator list
    Put the preprocessor conditions inside the struct initializers, since
    each branch defines the same global variable.
    
    Signed-off-by: Samuel Holland <samuel@sholland.org>

--- common/regulator_list.c
+++ common/regulator_list.c
@@ -10,65 +10,33 @@
 #include <regulator/gpio.h>
 #include <regulator/sy8106a.h>
 
-#if CONFIG(REGULATOR_AXP803)
-
 const struct regulator_handle cpu_supply = {
+#if CONFIG(REGULATOR_AXP803)
 	.dev = &axp803_regulator.dev,
 	.id  = AXP803_REGL_DCDC2,
-};
-
 #elif CONFIG(REGULATOR_AXP805)
-
-const struct regulator_handle cpu_supply = {
 	.dev = &axp805_regulator.dev,
 	.id  = AXP805_REGL_DCDCA,
-};
-
 #elif CONFIG(REGULATOR_SY8106A)
-
-const struct regulator_handle cpu_supply = {
 	.dev = &sy8106a.dev,
 	.id  = SY8106A_REGL_VOUT,
-};
-
 #elif CONFIG(REGULATOR_GPIO_CPU)
-
-const struct regulator_handle cpu_supply = {
 	.dev = &gpio_cpu_regulator.dev,
-};
-
 #else
-
-const struct regulator_handle cpu_supply = {
 	.dev = NULL,
-};
-
 #endif
-
-#if CONFIG(REGULATOR_AXP803)
+};
 
 const struct regulator_handle dram_supply = {
+#if CONFIG(REGULATOR_AXP803)
 	.dev = &axp803_regulator.dev,
 	.id  = AXP803_REGL_DCDC5,
-};
-
 #elif CONFIG(REGULATOR_AXP805)
-
-const struct regulator_handle dram_supply = {
 	.dev = &axp805_regulator.dev,
 	.id  = AXP805_REGL_DCDCE,
-};
-
 #elif CONFIG(REGULATOR_GPIO_DRAM)
-
-const struct regulator_handle dram_supply = {
 	.dev = &gpio_dram_regulator.dev,
-};
-
 #else
-
-const struct regulator_handle dram_supply = {
 	.dev = NULL,
-};
-
 #endif
+};
