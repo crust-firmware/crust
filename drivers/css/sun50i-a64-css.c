45cf0c48b93126c2d8928764a5fdb83b127dab42
Samuel Holland <samuel@sholland.org>
2021-02-27 18:55:59 -0600

    drivers: css: Dedent CSS driver functions
    In most cases, the firmware only needs to do program the hardware when
    the deeper state is below some boundary, currently SCPI_CSS_OFF. Instead
    of putting the whole function inside an `if` statement, return early if
    the deeper state is above that boundary. This allows unwrapping some
    long lines.
    
    `git show -b --word-diff=color` shows there is no functional change.
    
    Signed-off-by: Samuel Holland <samuel@sholland.org>

--- drivers/css/sun50i-a64-css.c
+++ drivers/css/sun50i-a64-css.c
@@ -18,115 +18,110 @@ static uint32_t rvba;
 void
 css_resume_cluster(uint32_t cluster UNUSED, uint32_t old_state)
 {
-	if (old_state == SCPI_CSS_OFF) {
-		/* Apply power to the cluster power domain. */
-		css_set_power_switch(C0_CPUn_PWR_SWITCH_REG(0), true);
-		/* Release the cluster output clamps. */
-		mmio_clr_32(C0_PWROFF_GATING_REG, C0_PWROFF_GATING);
-		udelay(1);
-		/* Deassert the CPU subsystem reset (active-low). */
-		mmio_write_32(CPU_SYS_RESET_REG, CPU_SYS_RESET);
-		udelay(1);
-		/* Deassert DBGPWRDUP for all cores. */
-		mmio_write_32(DBG_REG0, 0);
-		/* Assert all cluster and core resets (active-low). */
-		mmio_write_32(C0_RST_CTRL_REG, 0);
-		/* Enable hardware L2 cache flush (active-low). */
-		mmio_clr_32(C0_CTRL_REG0, C0_CTRL_REG0_L2RSTDISABLE);
-		/* Put the cluster back into coherency (deassert ACINACTM). */
-		mmio_clr_32(C0_CTRL_REG1, C0_CTRL_REG1_ACINACTM);
-		/* Deassert all cluster resets (active-low). */
-		mmio_write_32(C0_RST_CTRL_REG, C0_RST_CTRL_REG_MASK);
-		/* Restore the reset vector base addresses for all cores. */
-		for (uint32_t i = 0; i < css_get_core_count(cluster); ++i)
-			mmio_write_32(RVBA_LO_REG(i), rvba);
-	}
+	if (old_state < SCPI_CSS_OFF)
+		return;
+
+	/* Apply power to the cluster power domain. */
+	css_set_power_switch(C0_CPUn_PWR_SWITCH_REG(0), true);
+	/* Release the cluster output clamps. */
+	mmio_clr_32(C0_PWROFF_GATING_REG, C0_PWROFF_GATING);
+	udelay(1);
+	/* Deassert the CPU subsystem reset (active-low). */
+	mmio_write_32(CPU_SYS_RESET_REG, CPU_SYS_RESET);
+	udelay(1);
+	/* Deassert DBGPWRDUP for all cores. */
+	mmio_write_32(DBG_REG0, 0);
+	/* Assert all cluster and core resets (active-low). */
+	mmio_write_32(C0_RST_CTRL_REG, 0);
+	/* Enable hardware L2 cache flush (active-low). */
+	mmio_clr_32(C0_CTRL_REG0, C0_CTRL_REG0_L2RSTDISABLE);
+	/* Put the cluster back into coherency (deassert ACINACTM). */
+	mmio_clr_32(C0_CTRL_REG1, C0_CTRL_REG1_ACINACTM);
+	/* Deassert all cluster resets (active-low). */
+	mmio_write_32(C0_RST_CTRL_REG, C0_RST_CTRL_REG_MASK);
+	/* Restore the reset vector base addresses for all cores. */
+	for (uint32_t i = 0; i < css_get_core_count(cluster); ++i)
+		mmio_write_32(RVBA_LO_REG(i), rvba);
 }
 
 void
 css_suspend_cluster(uint32_t cluster UNUSED, uint32_t new_state)
 {
-	if (new_state == SCPI_CSS_OFF) {
-		/* Save the power-on reset vector base address from core 0. */
-		rvba = mmio_read_32(RVBA_LO_REG(0));
-		/* Assert L2FLUSHREQ to clean the cluster L2 cache. */
-		mmio_set_32(C0_CTRL_REG2, C0_CTRL_REG2_L2FLUSHREQ);
-		/* Wait for L2FLUSHDONE to go high. */
-		mmio_poll_32(L2_STATUS_REG, L2_STATUS_REG_L2FLUSHDONE);
-		/* Deassert L2FLUSHREQ. */
-		mmio_clr_32(C0_CTRL_REG2, C0_CTRL_REG2_L2FLUSHREQ);
-		/* Remove the cluster from coherency (assert ACINACTM). */
-		mmio_set_32(C0_CTRL_REG1, C0_CTRL_REG1_ACINACTM);
-		/* Wait for the cluster (L2 cache) to be idle. */
-		mmio_poll_32(C0_CPU_STATUS_REG,
-		             C0_CPU_STATUS_REG_STANDBYWFIL2);
-		/* Assert all cluster resets (active-low). */
-		mmio_write_32(C0_RST_CTRL_REG, 0);
-		/* Assert the CPU subsystem reset (active-low). */
-		mmio_write_32(CPU_SYS_RESET_REG, 0);
-		udelay(1);
-		/* Activate the cluster output clamps. */
-		mmio_set_32(C0_PWROFF_GATING_REG, C0_PWROFF_GATING);
-		/* Remove power from the cluster power domain. */
-		css_set_power_switch(C0_CPUn_PWR_SWITCH_REG(0), false);
-	}
+	if (new_state < SCPI_CSS_OFF)
+		return;
+
+	/* Save the power-on reset vector base address from core 0. */
+	rvba = mmio_read_32(RVBA_LO_REG(0));
+	/* Assert L2FLUSHREQ to clean the cluster L2 cache. */
+	mmio_set_32(C0_CTRL_REG2, C0_CTRL_REG2_L2FLUSHREQ);
+	/* Wait for L2FLUSHDONE to go high. */
+	mmio_poll_32(L2_STATUS_REG, L2_STATUS_REG_L2FLUSHDONE);
+	/* Deassert L2FLUSHREQ. */
+	mmio_clr_32(C0_CTRL_REG2, C0_CTRL_REG2_L2FLUSHREQ);
+	/* Remove the cluster from coherency (assert ACINACTM). */
+	mmio_set_32(C0_CTRL_REG1, C0_CTRL_REG1_ACINACTM);
+	/* Wait for the cluster (L2 cache) to be idle. */
+	mmio_poll_32(C0_CPU_STATUS_REG, C0_CPU_STATUS_REG_STANDBYWFIL2);
+	/* Assert all cluster resets (active-low). */
+	mmio_write_32(C0_RST_CTRL_REG, 0);
+	/* Assert the CPU subsystem reset (active-low). */
+	mmio_write_32(CPU_SYS_RESET_REG, 0);
+	udelay(1);
+	/* Activate the cluster output clamps. */
+	mmio_set_32(C0_PWROFF_GATING_REG, C0_PWROFF_GATING);
+	/* Remove power from the cluster power domain. */
+	css_set_power_switch(C0_CPUn_PWR_SWITCH_REG(0), false);
 }
 
 void
 css_resume_core(uint32_t cluster UNUSED, uint32_t core, uint32_t old_state)
 {
-	if (old_state == SCPI_CSS_OFF) {
-		/* Assert core reset (active-low). */
-		mmio_clr_32(C0_RST_CTRL_REG, C0_RST_CTRL_REG_nCORERESET(core));
-		/* Assert core power-on reset (active-low). */
-		mmio_clr_32(C0_PWRON_RESET_REG,
-		            C0_PWRON_RESET_REG_nCPUPORESET(core));
-		/* Program the core to start in AArch64 mode. */
-		mmio_set_32(C0_CTRL_REG0, C0_CTRL_REG0_AA64nAA32(core));
-		/* Core 0 does not have a separate power domain. */
-		if (core > 0) {
-			/* Turn on power to the core power domain. */
-			css_set_power_switch(C0_CPUn_PWR_SWITCH_REG(core),
-			                     true);
-			/* Release the core output clamps. */
-			mmio_clr_32(C0_PWROFF_GATING_REG,
-			            C0_CPUn_PWROFF_GATING(core));
-		}
-		/* Deassert core power-on reset (active-low). */
-		mmio_set_32(C0_PWRON_RESET_REG,
-		            C0_PWRON_RESET_REG_nCPUPORESET(core));
-		/* Deassert core reset (active-low). */
-		mmio_set_32(C0_RST_CTRL_REG, C0_RST_CTRL_REG_nCORERESET(core));
-		/* Assert DBGPWRDUP (allow debug access to the core). */
-		mmio_set_32(DBG_REG0, DBG_REG0_DBGPWRDUP(core));
+	if (old_state < SCPI_CSS_OFF)
+		return;
+
+	/* Assert core reset (active-low). */
+	mmio_clr_32(C0_RST_CTRL_REG, C0_RST_CTRL_REG_nCORERESET(core));
+	/* Assert core power-on reset (active-low). */
+	mmio_clr_32(C0_PWRON_RESET_REG, C0_PWRON_RESET_REG_nCPUPORESET(core));
+	/* Program the core to start in AArch64 mode. */
+	mmio_set_32(C0_CTRL_REG0, C0_CTRL_REG0_AA64nAA32(core));
+	/* Core 0 does not have a separate power domain. */
+	if (core > 0) {
+		/* Turn on power to the core power domain. */
+		css_set_power_switch(C0_CPUn_PWR_SWITCH_REG(core), true);
+		/* Release the core output clamps. */
+		mmio_clr_32(C0_PWROFF_GATING_REG, C0_CPUn_PWROFF_GATING(core));
 	}
+	/* Deassert core power-on reset (active-low). */
+	mmio_set_32(C0_PWRON_RESET_REG, C0_PWRON_RESET_REG_nCPUPORESET(core));
+	/* Deassert core reset (active-low). */
+	mmio_set_32(C0_RST_CTRL_REG, C0_RST_CTRL_REG_nCORERESET(core));
+	/* Assert DBGPWRDUP (allow debug access to the core). */
+	mmio_set_32(DBG_REG0, DBG_REG0_DBGPWRDUP(core));
 }
 
 void
 css_suspend_core(uint32_t cluster UNUSED, uint32_t core, uint32_t new_state)
 {
-	if (new_state == SCPI_CSS_OFF) {
-		/* Wait for the core to be in WFI and ready to shut down. */
-		mmio_poll_32(C0_CPU_STATUS_REG,
-		             C0_CPU_STATUS_REG_STANDBYWFI(core));
-		/* Deassert DBGPWRDUP (prevent debug access to the core). */
-		mmio_clr_32(DBG_REG0, DBG_REG0_DBGPWRDUP(core));
-		/* Core 0 does not have a separate power domain. */
-		if (core > 0) {
-			/* Activate the core output clamps. */
-			mmio_set_32(C0_PWROFF_GATING_REG,
-			            C0_CPUn_PWROFF_GATING(core));
-		}
-		/* Assert core reset (active-low). */
-		mmio_clr_32(C0_RST_CTRL_REG, C0_RST_CTRL_REG_nCORERESET(core));
-		/* Assert core power-on reset (active-low). */
-		mmio_clr_32(C0_PWRON_RESET_REG,
-		            C0_PWRON_RESET_REG_nCPUPORESET(core));
-		/* Core 0 does not have a separate power domain. */
-		if (core > 0) {
-			/* Remove power from the core power domain. */
-			css_set_power_switch(C0_CPUn_PWR_SWITCH_REG(core),
-			                     false);
-		}
+	if (new_state < SCPI_CSS_OFF)
+		return;
+
+	/* Wait for the core to be in WFI and ready to shut down. */
+	mmio_poll_32(C0_CPU_STATUS_REG, C0_CPU_STATUS_REG_STANDBYWFI(core));
+	/* Deassert DBGPWRDUP (prevent debug access to the core). */
+	mmio_clr_32(DBG_REG0, DBG_REG0_DBGPWRDUP(core));
+	/* Core 0 does not have a separate power domain. */
+	if (core > 0) {
+		/* Activate the core output clamps. */
+		mmio_set_32(C0_PWROFF_GATING_REG, C0_CPUn_PWROFF_GATING(core));
+	}
+	/* Assert core reset (active-low). */
+	mmio_clr_32(C0_RST_CTRL_REG, C0_RST_CTRL_REG_nCORERESET(core));
+	/* Assert core power-on reset (active-low). */
+	mmio_clr_32(C0_PWRON_RESET_REG, C0_PWRON_RESET_REG_nCPUPORESET(core));
+	/* Core 0 does not have a separate power domain. */
+	if (core > 0) {
+		/* Remove power from the core power domain. */
+		css_set_power_switch(C0_CPUn_PWR_SWITCH_REG(core), false);
 	}
 }
--- drivers/css/sun8i-h3-css.c
+++ drivers/css/sun8i-h3-css.c
@@ -14,38 +14,39 @@
 void
 css_resume_core(uint32_t cluster UNUSED, uint32_t core, uint32_t old_state)
 {
-	if (old_state == SCPI_CSS_OFF) {
-		/* Assert core reset and power-on reset (active-low). */
-		mmio_write_32(CPUn_RST_CTRL_REG(core), 0);
-		/* Enable hardware L1 cache flush (active-low). */
-		mmio_clr_32(GEN_CTRL_REG, GEN_CTRL_REG_L1RSTDISABLE(core));
-		/* Turn on power to the core power domain. */
-		css_set_power_switch(C0_CPUn_PWR_SWITCH_REG(core), true);
-		/* Release the core output clamps. */
-		mmio_clr_32(C0_PWROFF_GATING_REG, C0_CPUn_PWROFF_GATING(core));
-		/* Deassert core reset and power-on reset (active-low). */
-		mmio_write_32(CPUn_RST_CTRL_REG(core),
-		              CPUn_RST_CTRL_REG_nCORERESET |
-		              CPUn_RST_CTRL_REG_nCPUPORESET);
-		/* Assert DBGPWRDUP (allow debug access to the core). */
-		mmio_set_32(DBG_CTRL_REG1, DBG_CTRL_REG1_DBGPWRDUP(core));
-	}
+	if (old_state < SCPI_CSS_OFF)
+		return;
+
+	/* Assert core reset and power-on reset (active-low). */
+	mmio_write_32(CPUn_RST_CTRL_REG(core), 0);
+	/* Enable hardware L1 cache flush (active-low). */
+	mmio_clr_32(GEN_CTRL_REG, GEN_CTRL_REG_L1RSTDISABLE(core));
+	/* Turn on power to the core power domain. */
+	css_set_power_switch(C0_CPUn_PWR_SWITCH_REG(core), true);
+	/* Release the core output clamps. */
+	mmio_clr_32(C0_PWROFF_GATING_REG, C0_CPUn_PWROFF_GATING(core));
+	/* Deassert core reset and power-on reset (active-low). */
+	mmio_write_32(CPUn_RST_CTRL_REG(core),
+	              CPUn_RST_CTRL_REG_nCORERESET |
+	              CPUn_RST_CTRL_REG_nCPUPORESET);
+	/* Assert DBGPWRDUP (allow debug access to the core). */
+	mmio_set_32(DBG_CTRL_REG1, DBG_CTRL_REG1_DBGPWRDUP(core));
 }
 
 void
 css_suspend_core(uint32_t cluster UNUSED, uint32_t core, uint32_t new_state)
 {
-	if (new_state == SCPI_CSS_OFF) {
-		/* Wait for the core to be in WFI and ready to shut down. */
-		mmio_poll_32(CPUn_STATUS_REG(core),
-		             CPUn_STATUS_REG_STANDBYWFI);
-		/* Deassert DBGPWRDUP (prevent debug access to the core). */
-		mmio_clr_32(DBG_CTRL_REG1, DBG_CTRL_REG1_DBGPWRDUP(core));
-		/* Assert core reset and power-on reset (active-low). */
-		mmio_write_32(CPUn_RST_CTRL_REG(core), 0);
-		/* Activate the core output clamps. */
-		mmio_set_32(C0_PWROFF_GATING_REG, C0_CPUn_PWROFF_GATING(core));
-		/* Remove power from the core power domain. */
-		css_set_power_switch(C0_CPUn_PWR_SWITCH_REG(core), false);
-	}
+	if (new_state < SCPI_CSS_OFF)
+		return;
+
+	/* Wait for the core to be in WFI and ready to shut down. */
+	mmio_poll_32(CPUn_STATUS_REG(core), CPUn_STATUS_REG_STANDBYWFI);
+	/* Deassert DBGPWRDUP (prevent debug access to the core). */
+	mmio_clr_32(DBG_CTRL_REG1, DBG_CTRL_REG1_DBGPWRDUP(core));
+	/* Assert core reset and power-on reset (active-low). */
+	mmio_write_32(CPUn_RST_CTRL_REG(core), 0);
+	/* Activate the core output clamps. */
+	mmio_set_32(C0_PWROFF_GATING_REG, C0_CPUn_PWROFF_GATING(core));
+	/* Remove power from the core power domain. */
+	css_set_power_switch(C0_CPUn_PWR_SWITCH_REG(core), false);
 }
