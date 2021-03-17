crust-firmware/crust

commit c77d2a43e9e32d2333bd6454ea931f64d6b20d36

system: Power down VCC-PLL when possible
Because the regulator supplying VCC-PLL is often shared with other power
domains, the rules for powering it down are somewhat complicated.

- On all known A64/H3/H5 boards, VCC-PLL shares a regulator with AVCC,
  which is needed for LRADC wakeup. However, this is mostly irrelevant
  for H3/H5, because the regulator on those boards also powers VCC-RTC
  and thus is always on.
- On some H6 boards, BLDO1 provides 1v8 power to the DRAM, so it can
  only be disabled during shutdown, when DRAM contents are discarded.
  Other boards get this 1v8 DRAM power from the always-on VCC-RTC/VINT
  supply, so they can power down BLDO1 during sleep as well.

No GPIO regulator instance is needed for this power domain, since it is
only switchable on boards with PMICs.

Signed-off-by: Samuel Holland <samuel@sholland.org>

common/system.c
@@ -194,11 +194,16 @@ system_state_machine(uint32_t exception)
 
 			/* Turn off all unnecessary power domains. */
 			regulator_disable(&cpu_supply);
-			if (system_state == SS_SHUTDOWN) {
+			if (system_state == SS_SHUTDOWN)
 				regulator_disable(&dram_supply);
-				if (suspend_depth >= SD_VDD_SYS)
-					regulator_disable(&vdd_sys_supply);
-			}
+			if (suspend_depth >= SD_OSC24M &&
+			    (!CONFIG(VCC_PLL_POWERS_AVCC) ||
+			     suspend_depth >= SD_AVCC) &&
+			    (!CONFIG(VCC_PLL_POWERS_DRAM) ||
+			     system_state == SS_SHUTDOWN))
+				regulator_disable(&vcc_pll_supply);
+			if (suspend_depth >= SD_VDD_SYS)
+				regulator_disable(&vdd_sys_supply);
 
 			/*
 			 * The regulator provider is often part of the same
@@ -232,10 +237,9 @@ system_state_machine(uint32_t exception)
 			 * If it fails, manually turn the regulators back on.
 			 */
 			if (!(pmic = pmic_get()) || pmic_resume(pmic)) {
-				if (system_state == SS_PRE_RESET) {
-					regulator_enable(&vdd_sys_supply);
-					regulator_enable(&dram_supply);
-				}
+				regulator_enable(&vdd_sys_supply);
+				regulator_enable(&vcc_pll_supply);
+				regulator_enable(&dram_supply);
 				regulator_enable(&cpu_supply);
 			}
 			device_put(pmic);

configs/pine_h64_defconfig
@@ -1,3 +1,4 @@
 CONFIG_PLATFORM_H6=y
 CONFIG_CIR=y
 CONFIG_MFD_AXP20X=y
+# CONFIG_VCC_PLL_POWERS_DRAM is not set

drivers/regulator/Kconfig
@@ -87,3 +87,29 @@ config REGULATOR_SY8106A
 	depends on I2C
 
 endmenu
+
+if REGULATOR_AXP803 || REGULATOR_AXP805
+
+config VCC_PLL_POWERS_AVCC
+	bool "VCC-PLL also powers the AVCC power domain"
+	default PLATFORM_A64 || PLATFORM_H3
+	help
+		This option will keep the VCC-PLL regulator enabled as
+		long as AVCC is needed by a wakeup source.
+
+		Disable this option, if possible, after verifying it is
+		not needed on your board. If you are unsure, keep the
+		default value.
+
+config VCC_PLL_POWERS_DRAM
+	bool "VCC-PLL also powers the DRAM"
+	default PLATFORM_H6
+	help
+		This option will keep the VCC-PLL regulator enabled as
+		long as the DRAM is in use (i.e. during system sleep).
+
+		Disable this option, if possible, after verifying it is
+		not needed on your board. If you are unsure, keep the
+		default value.
+
+endif

common/regulator_list.c
@@ -40,6 +40,18 @@ const struct regulator_handle dram_supply = {
 #endif
 };
 
+const struct regulator_handle vcc_pll_supply = {
+#if CONFIG(REGULATOR_AXP803)
+	.dev = &axp803_regulator.dev,
+	.id  = AXP803_REGL_ALDO3,
+#elif CONFIG(REGULATOR_AXP805)
+	.dev = &axp805_regulator.dev,
+	.id  = AXP805_REGL_BLDO1,
+#else
+	.dev = NULL,
+#endif
+};
+
 const struct regulator_handle vdd_sys_supply = {
 #if CONFIG(REGULATOR_AXP803)
 	.dev = &axp803_regulator.dev,

include/common/regulator_list.h
@@ -19,6 +19,11 @@ extern const struct regulator_handle cpu_supply;
 extern const struct regulator_handle dram_supply;
 
 /**
+ * The regulator supplying VCC-PLL.
+ */
+extern const struct regulator_handle vcc_pll_supply;
+
+/**
  * The regulator supplying VDD-SYS.
  */
 extern const struct regulator_handle vdd_sys_supply;

commit de2474d8e2bf75240c02e2651b5aa48d88b84fb2

system: Only power down VCC-PLL during shutdown
In addition to triggering a spurious LRADC keypress, powering down the
A64's AVCC breaks audio. While this is likely fixable, doing so is not
worthwile. The reduction in power usage from powering down AVCC/VCC-PLL
is very small. (Allwinner documented 60 uA. I measured 300 uA. Either
way, it is not much.) And as they also documented, cycling VCC-PLL
increases the time needed to resume -- in our case, by almost 5x.

However, none of those downsides apply during shutdown. Audio and the
LRADC do not need to work then, and the PLL reinitialzation is handled
by the SoC's reset procedure. By continuing to power down AVCC/VCC-PLL
during shutdown on the A64, its logic now matches the H6 logic. This
allows dropping the Kconfig options and simplifying the code a bit.

Fixes: c77d2a43e9e3 ("system: Power down VCC-PLL when possible")
Reported-by: Dalton Durst <dalton@ubports.com>
Signed-off-by: Samuel Holland <samuel@sholland.org>

common/system.c
@@ -70,7 +70,8 @@ select_suspend_depth(uint8_t current_state)
 	/* Bail if the DRAM controller or peripherals need running clocks. */
 	if (!CONFIG(HAVE_DRAM_SUSPEND) || clock_active(&osc24m))
 		return SD_NONE;
-	if (irq_needs_avcc())
+	/* Wakeup sources needing AVCC are only supported while asleep. */
+	if (current_state != SS_SHUTDOWN && irq_needs_avcc())
 		return SD_OSC24M;
 	if (current_state != SS_SHUTDOWN || irq_needs_vdd_sys())
 		return SD_AVCC;
@@ -197,16 +198,13 @@ system_state_machine(uint32_t exception)
 
 			/* Turn off all unnecessary power domains. */
 			regulator_disable(&cpu_supply);
-			if (system_state == SS_SHUTDOWN)
+			if (system_state == SS_SHUTDOWN) {
 				regulator_disable(&dram_supply);
-			if (suspend_depth >= SD_OSC24M &&
-			    (!CONFIG(VCC_PLL_POWERS_AVCC) ||
-			     suspend_depth >= SD_AVCC) &&
-			    (!CONFIG(VCC_PLL_POWERS_DRAM) ||
-			     system_state == SS_SHUTDOWN))
-				regulator_disable(&vcc_pll_supply);
-			if (suspend_depth >= SD_VDD_SYS)
-				regulator_disable(&vdd_sys_supply);
+				if (suspend_depth >= SD_OSC24M)
+					regulator_disable(&vcc_pll_supply);
+				if (suspend_depth >= SD_VDD_SYS)
+					regulator_disable(&vdd_sys_supply);
+			}
 
 			/*
 			 * The regulator provider is often part of the same
@@ -248,7 +246,7 @@ system_state_machine(uint32_t exception)
 			device_put(pmic);
 
 			/* Give regulator outputs time to rise. */
-			udelay(25000);
+			udelay(5000);
 
 			/* Restore SoC-internal power domains. */
 			r_ccu_resume();

configs/pine_h64_defconfig
@@ -1,4 +1,3 @@
 CONFIG_PLATFORM_H6=y
 CONFIG_CIR=y
 CONFIG_MFD_AXP20X=y
-# CONFIG_VCC_PLL_POWERS_DRAM is not set

drivers/regulator/Kconfig
@@ -87,29 +87,3 @@ config REGULATOR_SY8106A
 	depends on I2C
 
 endmenu
-
-if REGULATOR_AXP803 || REGULATOR_AXP805
-
-config VCC_PLL_POWERS_AVCC
-	bool "VCC-PLL also powers the AVCC power domain"
-	default PLATFORM_A64 || PLATFORM_H3
-	help
-		This option will keep the VCC-PLL regulator enabled as
-		long as AVCC is needed by a wakeup source.
-
-		Disable this option, if possible, after verifying it is
-		not needed on your board. If you are unsure, keep the
-		default value.
-
-config VCC_PLL_POWERS_DRAM
-	bool "VCC-PLL also powers the DRAM"
-	default PLATFORM_H6
-	help
-		This option will keep the VCC-PLL regulator enabled as
-		long as the DRAM is in use (i.e. during system sleep).
-
-		Disable this option, if possible, after verifying it is
-		not needed on your board. If you are unsure, keep the
-		default value.
-
-endif