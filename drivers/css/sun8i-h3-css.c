4bb70408ed81ccda88b2986232686e7935bb8840
Samuel Holland <samuel@sholland.org>
2021-10-10 13:22:37 -0500

    drivers: sun8i-h3-css: Add CSS suspend support
    This hooks up the CPU subsystem reset so power can be removed from the
    CSS while the system is suspended. The gating and power switch registers
    for CPU 0 appear to have no effect. Even when they are gated and off,
    deasserting the CSS reset causes CPU 0 to start executing instructions
    unless it is individually reset. The change to handle this matches the
    code for the A64 CSS.
    
    Signed-off-by: Samuel Holland <samuel@sholland.org>

--- drivers/css/sun8i-h3-css.c
+++ drivers/css/sun8i-h3-css.c
@@ -13,6 +13,26 @@
 #include "css.h"
 
 void
+css_suspend_css(uint32_t new_state)
+{
+	if (new_state < SCPI_CSS_OFF)
+		return;
+
+	/* Assert the CPU subsystem reset (active-low). */
+	mmio_write_32(CPU_SYS_RESET_REG, 0);
+}
+
+void
+css_resume_css(uint32_t old_state)
+{
+	if (old_state < SCPI_CSS_OFF)
+		return;
+
+	/* Deassert the CPU subsystem reset (active-low). */
+	mmio_write_32(CPU_SYS_RESET_REG, CPU_SYS_RESET);
+}
+
+void
 css_suspend_cluster(uint32_t cluster, uint32_t new_state)
 {
 	if (new_state < SCPI_CSS_RETENTION)
@@ -42,10 +62,16 @@ css_suspend_core(uint32_t cluster UNUSED, uint32_t core, uint32_t new_state)
 	mmio_poll_32(CPUn_STATUS_REG(core), CPUn_STATUS_REG_STANDBYWFI);
 	/* Deassert DBGPWRDUP (prevent debug access to the core). */
 	mmio_clr_32(DBG_CTRL_REG1, DBG_CTRL_REG1_DBGPWRDUP(core));
-	/* Activate the core output clamps. */
-	mmio_set_32(C0_PWROFF_GATING_REG, C0_CPUn_PWROFF_GATING(core));
-	/* Remove power from the core power domain. */
-	css_set_power_switch(C0_CPUn_PWR_SWITCH_REG(core), false);
+	/* Core 0 does not have a separate power domain. */
+	if (core > 0) {
+		/* Activate the core output clamps. */
+		mmio_set_32(C0_PWROFF_GATING_REG, C0_CPUn_PWROFF_GATING(core));
+		/* Remove power from the core power domain. */
+		css_set_power_switch(C0_CPUn_PWR_SWITCH_REG(core), false);
+	} else {
+		/* Assert core reset and power-on reset (active-low). */
+		mmio_write_32(CPUn_RST_CTRL_REG(core), 0);
+	}
 }
 
 void
@@ -54,12 +80,15 @@ css_resume_core(uint32_t cluster UNUSED, uint32_t core, uint32_t old_state)
 	if (old_state < SCPI_CSS_OFF)
 		return;
 
-	/* Assert core reset and power-on reset (active-low). */
-	mmio_write_32(CPUn_RST_CTRL_REG(core), 0);
-	/* Turn on power to the core power domain. */
-	css_set_power_switch(C0_CPUn_PWR_SWITCH_REG(core), true);
-	/* Release the core output clamps. */
-	mmio_clr_32(C0_PWROFF_GATING_REG, C0_CPUn_PWROFF_GATING(core));
+	/* Core 0 does not have a separate power domain. */
+	if (core > 0) {
+		/* Assert core reset and power-on reset (active-low). */
+		mmio_write_32(CPUn_RST_CTRL_REG(core), 0);
+		/* Turn on power to the core power domain. */
+		css_set_power_switch(C0_CPUn_PWR_SWITCH_REG(core), true);
+		/* Release the core output clamps. */
+		mmio_clr_32(C0_PWROFF_GATING_REG, C0_CPUn_PWROFF_GATING(core));
+	}
 	/* Deassert core reset and power-on reset (active-low). */
 	mmio_write_32(CPUn_RST_CTRL_REG(core),
 	              CPUn_RST_CTRL_REG_nCORERESET |
