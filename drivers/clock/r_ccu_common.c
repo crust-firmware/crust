crust-firmware/crust

commit 07183cf26f0061555babd713d307744c8fb94f27

drivers: ccu: Split updates to PLL_CTRL_REG1
Depending on which pads the 24 MHz crystal is connected to, the
PLL_CTRL_REG1_CRYSTAL_EN bit may not have any effect. In that case,
there is no need for the extra delays or register writes.

Split the two register writes out of the helper function so one of them
can be compiled out. Currently, the helper for writing PLL_CTRL_REG1
does everything at once in an attempt to reduce code size.  In fact,
performing the two writes separately actually produces a smaller binary.

Signed-off-by: Samuel Holland <samuel@sholland.org>

drivers/clock/r_ccu_common.c
@@ -24,20 +24,17 @@ static uint32_t osc16m_rate = 16000000U;
 DEFINE_FIXED_RATE(r_ccu_common_get_osc16m_rate, osc16m_rate)
 
 /**
- * Write two consecutive values to PLL_CTRL_REG1 with a delay in between.
+ * Write a value to the lockable bits in PLL_CTRL_REG1.
  */
 static void
-r_ccu_common_update_pll_ctrl_reg1(uint32_t v1, uint32_t delay, uint32_t v2)
+write_pll_ctrl_reg1(uint32_t new)
 {
 	uint32_t val = mmio_read_32(PLL_CTRL_REG1) & ~PLL_CTRL_REG1_MASK;
 
-	/* Step 1: unlock if locked, otherwise write first value. */
-	mmio_write_32(PLL_CTRL_REG1, val | v1 | PLL_CTRL_REG1_KEY);
-	/* Step 2: write first value if just unlocked, otherwise write same. */
-	mmio_write_32(PLL_CTRL_REG1, val | v1 | PLL_CTRL_REG1_KEY);
-	udelay(delay);
-	/* Step 3: write second value and lock. */
-	mmio_write_32(PLL_CTRL_REG1, val | v2);
+	/* Unlock if locked, otherwise write value. */
+	mmio_write_32(PLL_CTRL_REG1, val | new | PLL_CTRL_REG1_KEY);
+	/* Write value if just unlocked, otherwise write same; lock. */
+	mmio_write_32(PLL_CTRL_REG1, val | new);
 }
 
 void
@@ -46,7 +43,9 @@ r_ccu_common_suspend(void)
 	if (!CONFIG(SUSPEND_OSC24M))
 		return;
 
-	r_ccu_common_update_pll_ctrl_reg1(PLL_CTRL_REG1_LDO_EN, 1, 0);
+	write_pll_ctrl_reg1(PLL_CTRL_REG1_LDO_EN);
+	udelay(1);
+	write_pll_ctrl_reg1(0);
 	mmio_set_32(VDD_SYS_PWROFF_GATING_REG,
 	            CONFIG(GATE_VDD_SYS) * VDD_CPUS_GATING | VCC_PLL_GATING);
 }
@@ -62,9 +61,9 @@ r_ccu_common_resume(void)
 
 	mmio_clr_32(VDD_SYS_PWROFF_GATING_REG,
 	            CONFIG(GATE_VDD_SYS) * VDD_CPUS_GATING | VCC_PLL_GATING);
-	r_ccu_common_update_pll_ctrl_reg1(PLL_CTRL_REG1_LDO_EN, 2000,
-	                                  PLL_CTRL_REG1_CRYSTAL_EN |
-	                                  PLL_CTRL_REG1_LDO_EN);
+	write_pll_ctrl_reg1(PLL_CTRL_REG1_LDO_EN);
+	udelay(2000);
+	write_pll_ctrl_reg1(PLL_CTRL_REG1_CRYSTAL_EN | PLL_CTRL_REG1_LDO_EN);
 }
 
 void WEAK ATTRIBUTE(alias("r_ccu_common_resume"))

commit 017e4fd9fc94d306302c7e782cc2ed36021a1478

drivers: ccu: Do not turn off X24M during resume
commit 07183cf26f00 ("drivers: ccu: Split updates to PLL_CTRL_REG1")
introduced an issue where resuming the system would unconditionally turn
off the X24M crystal for 2 ms. This applied even if resuming from depth
zero, where PLL_CTRL_REG1 was not touched during suspend.

Since this can cause some instability, add a check to skip writing to
PLL_CTRL_REG1 if the bits are already set.

Fixes: 07183cf26f00 ("drivers: ccu: Split updates to PLL_CTRL_REG1")
Signed-off-by: Samuel Holland <samuel@sholland.org>

drivers/clock/r_ccu_common.c
@@ -75,11 +75,13 @@ r_ccu_common_resume(void)
 	 */
 	mmio_write_32(VDD_SYS_RESET_REG, VDD_SYS_RESET);
 	mmio_clr_32(VDD_SYS_PWROFF_GATING_REG, VDD_CPUS_GATING | AVCC_GATING);
-	write_pll_ctrl_reg1(PLL_CTRL_REG1_LDO_EN);
-	if (CONFIG(OSC24M_SRC_X24M)) {
-		udelay(2000);
-		write_pll_ctrl_reg1(PLL_CTRL_REG1_CRYSTAL_EN |
-		                    PLL_CTRL_REG1_LDO_EN);
+	if (!mmio_get_32(PLL_CTRL_REG1, PLL_CTRL_REG1_LDO_EN)) {
+		write_pll_ctrl_reg1(PLL_CTRL_REG1_LDO_EN);
+		if (CONFIG(OSC24M_SRC_X24M)) {
+			udelay(2000);
+			write_pll_ctrl_reg1(PLL_CTRL_REG1_CRYSTAL_EN |
+			                    PLL_CTRL_REG1_LDO_EN);
+		}
 	}
 }
 