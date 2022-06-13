8258f5472e7d5a43a581207c2c64d682d7c8575a
Samuel Holland <samuel@sholland.org>
2022-06-12 21:52:52 -0500

    drivers: serial: Add support for the A23/A33 UARTs
    This uses the UART0 mux (Port F) that is common between A23 and A33.
    
    Signed-off-by: Samuel Holland <samuel@sholland.org>

---
+++ drivers/serial/sun8i-a23-uart.c
@@ -0,0 +1,180 @@
+/*
+ * Copyright © 2022 The Crust Firmware Authors.
+ * SPDX-License-Identifier: BSD-3-Clause OR GPL-2.0-only
+ */
+
+#include <clock/ccu.h>
+#include <gpio/sunxi-gpio.h>
+#include <platform/devices.h>
+
+#include "uart.h"
+
+#if CONFIG(SERIAL_DEV_UART0)
+
+const struct simple_device uart = {
+	.dev = {
+		.name  = "uart0",
+		.drv   = &uart_driver,
+		.state = DEVICE_STATE_INIT,
+	},
+	.clock = { .dev = &ccu.dev, .id = CLK_BUS_UART0 },
+	.pins  = SIMPLE_DEVICE_PINS_INIT {
+		{
+			.dev   = &pio.dev,
+			.id    = SUNXI_GPIO_PIN(5, 2), /* PF2 */
+			.drive = DRIVE_10mA,
+			.mode  = 3,
+			.pull  = PULL_NONE,
+		},
+		{
+			.dev   = &pio.dev,
+			.id    = SUNXI_GPIO_PIN(5, 4), /* PF4 */
+			.drive = DRIVE_10mA,
+			.mode  = 3,
+			.pull  = PULL_UP,
+		},
+	},
+	.regs = DEV_UART0,
+};
+
+#elif CONFIG(SERIAL_DEV_UART1)
+
+const struct simple_device uart = {
+	.dev = {
+		.name  = "uart1",
+		.drv   = &uart_driver,
+		.state = DEVICE_STATE_INIT,
+	},
+	.clock = { .dev = &ccu.dev, .id = CLK_BUS_UART1 },
+	.pins  = SIMPLE_DEVICE_PINS_INIT {
+		{
+			.dev   = &pio.dev,
+			.id    = SUNXI_GPIO_PIN(6, 6), /* PG6 */
+			.drive = DRIVE_10mA,
+			.mode  = 2,
+			.pull  = PULL_NONE,
+		},
+		{
+			.dev   = &pio.dev,
+			.id    = SUNXI_GPIO_PIN(6, 7), /* PG7 */
+			.drive = DRIVE_10mA,
+			.mode  = 2,
+			.pull  = PULL_UP,
+		},
+	},
+	.regs = DEV_UART1,
+};
+
+#elif CONFIG(SERIAL_DEV_UART2)
+
+const struct simple_device uart = {
+	.dev = {
+		.name  = "uart2",
+		.drv   = &uart_driver,
+		.state = DEVICE_STATE_INIT,
+	},
+	.clock = { .dev = &ccu.dev, .id = CLK_BUS_UART2 },
+	.pins  = SIMPLE_DEVICE_PINS_INIT {
+		{
+			.dev   = &pio.dev,
+			.id    = SUNXI_GPIO_PIN(1, 0), /* PB0 */
+			.drive = DRIVE_10mA,
+			.mode  = 2,
+			.pull  = PULL_NONE,
+		},
+		{
+			.dev   = &pio.dev,
+			.id    = SUNXI_GPIO_PIN(1, 1), /* PB1 */
+			.drive = DRIVE_10mA,
+			.mode  = 2,
+			.pull  = PULL_UP,
+		},
+	},
+	.regs = DEV_UART2,
+};
+
+#elif CONFIG(SERIAL_DEV_UART3)
+
+const struct simple_device uart = {
+	.dev = {
+		.name  = "uart3",
+		.drv   = &uart_driver,
+		.state = DEVICE_STATE_INIT,
+	},
+	.clock = { .dev = &ccu.dev, .id = CLK_BUS_UART3 },
+	.pins  = SIMPLE_DEVICE_PINS_INIT {
+		{
+			.dev   = &pio.dev,
+			.id    = SUNXI_GPIO_PIN(7, 6), /* PH4 */
+			.drive = DRIVE_10mA,
+			.mode  = 3,
+			.pull  = PULL_NONE,
+		},
+		{
+			.dev   = &pio.dev,
+			.id    = SUNXI_GPIO_PIN(7, 7), /* PH5 */
+			.drive = DRIVE_10mA,
+			.mode  = 3,
+			.pull  = PULL_UP,
+		},
+	},
+	.regs = DEV_UART3,
+};
+
+#elif CONFIG(SERIAL_DEV_UART4)
+
+const struct simple_device uart = {
+	.dev = {
+		.name  = "uart4",
+		.drv   = &uart_driver,
+		.state = DEVICE_STATE_INIT,
+	},
+	.clock = { .dev = &ccu.dev, .id = CLK_BUS_UART4 },
+	.pins  = SIMPLE_DEVICE_PINS_INIT {
+		{
+			.dev   = &pio.dev,
+			.id    = SUNXI_GPIO_PIN(0, 4), /* PA4 */
+			.drive = DRIVE_10mA,
+			.mode  = 3,
+			.pull  = PULL_NONE,
+		},
+		{
+			.dev   = &pio.dev,
+			.id    = SUNXI_GPIO_PIN(0, 5), /* PA5 */
+			.drive = DRIVE_10mA,
+			.mode  = 3,
+			.pull  = PULL_UP,
+		},
+	},
+	.regs = DEV_UART4,
+};
+
+#elif CONFIG(SERIAL_DEV_R_UART)
+
+const struct simple_device uart = {
+	.dev = {
+		.name  = "r_uart",
+		.drv   = &uart_driver,
+		.state = DEVICE_STATE_INIT,
+	},
+	.clock = { .dev = &r_ccu.dev, .id = CLK_BUS_R_UART },
+	.pins  = SIMPLE_DEVICE_PINS_INIT {
+		{
+			.dev   = &r_pio.dev,
+			.id    = SUNXI_GPIO_PIN(0, 2), /* PL2 */
+			.drive = DRIVE_10mA,
+			.mode  = 2,
+			.pull  = PULL_NONE,
+		},
+		{
+			.dev   = &r_pio.dev,
+			.id    = SUNXI_GPIO_PIN(0, 3), /* PL3 */
+			.drive = DRIVE_10mA,
+			.mode  = 2,
+			.pull  = PULL_UP,
+		},
+	},
+	.regs = DEV_R_UART,
+};
+
+#endif
